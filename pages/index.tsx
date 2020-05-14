6008f7dbf84c7f04ce62ee97d41c250456a9afb9
UMASHIBA <umashiba1@gmail.com>
2020-05-14 11:02:35 +0900

    [DEL]Initする時に作成された必要のないファイルを削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,3 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+const IndexPage = () => <main>Hello world</main>;
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
-
-export default IndexPage
+export default IndexPage;
