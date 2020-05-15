96a40933dff0f8416811bd625523b27ad69ba4b3
UMASHIBA <umashiba1@gmail.com>
2020-05-15 21:16:01 +0900

    [MOD]index.tsxをタダの白い画面に変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
-const IndexPage = () => <main>Hello world</main>;
+import styled from "styled-components";
+
+const Main = styled.main`
+  width: 100%;
+  height: 100%;
+`;
+
+const IndexPage = () => <Main></Main>;
 
 export default IndexPage;
