cc4fea6b5e3d13c015ffe4dc4fff224a0f6c37ff
UMASHIBA <umashiba1@gmail.com>
2020-05-15 23:49:05 +0900

    [MOD]styled-componentsを含めたSSRが行われるよう_document.tsxを変更

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,11 +6,34 @@ import Document, {
   Main,
   NextScript,
 } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () => {
+        return originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+      };
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
   render() {
     return (
