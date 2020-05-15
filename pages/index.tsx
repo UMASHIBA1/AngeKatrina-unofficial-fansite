f499f98a5fe7d4edf196a762a8f5a3f11e7f13ef
UMASHIBA <umashiba1@gmail.com>
2020-05-15 22:31:25 +0900

    [MOD]indexにMagicCircleを表示

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import styled from "styled-components";
+import MagicCircle from "../components/organisms/start_animation/MagicCircle";
 
 const Main = styled.main`
   width: 100%;
   height: 100%;
 `;
 
-const IndexPage = () => <Main></Main>;
+const IndexPage = () => (
+  <Main>
+    <MagicCircle />
+  </Main>
+);
 
 export default IndexPage;
