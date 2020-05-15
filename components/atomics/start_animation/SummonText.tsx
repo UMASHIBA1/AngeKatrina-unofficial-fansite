0a8aa2fc7c0ffd277b8a8ba0a5a159d18c58c5b2
UMASHIBA <umashiba1@gmail.com>
2020-05-16 00:21:21 +0900

    [MOD]SummonTextを要素の直径を得るように変更しかつ背景を追加

--- components/atomics/start_animation/SummonText.tsx
+++ components/atomics/start_animation/SummonText.tsx
@@ -1,19 +1,34 @@
 import React from "react";
 import styled from "styled-components";
+import { ANGE_BROWN, ANGE_RED } from "../../../constants/colors";
 import { HWT_MARDELL_FONT_PROP } from "../../../constants/cssProps";
-import { ANGE_RED } from "../../../constants/colors";
 
-const StyledSummonText = styled.span`
+const StyledSummonText = styled.div<{ diameter: number }>`
   ${HWT_MARDELL_FONT_PROP}
   color: ${ANGE_RED};
   position: absolute;
-  left: calc(50% - 68px);
-  top: calc(50% - 16px);
+  width: ${({ diameter }) => diameter}px;
+  height: ${({ diameter }) => diameter}px;
+  left: calc(50% - ${({ diameter }) => diameter / 2}px);
+  top: calc(50% - ${({ diameter }) => diameter / 2}px);
   font-size: 36px;
-`;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color ${ANGE_BROWN};
+  border-radius: 50%;
+  `;
 
-const SummonText: React.FC = () => {
-  return <StyledSummonText>Summon</StyledSummonText>;
+interface Props {
+  diameter: number;
+}
+
+const SummonText: React.FC<Props> = ({ diameter: Diameter }: Props) => {
+  return (
+    <StyledSummonText diameter={Diameter}>
+      <span>Summon</span>
+    </StyledSummonText>
+  );
 };
 
 export default SummonText;
