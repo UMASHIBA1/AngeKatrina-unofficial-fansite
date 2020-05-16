a5913a462c5638bc1bd192c5a62331998a402bca
UMASHIBA <umashiba1@gmail.com>
2020-05-17 02:34:08 +0900

    [MOD]toDeepInsetShadowを作成

--- styles/commonAnimation.ts
+++ styles/commonAnimation.ts
@@ -19,3 +19,14 @@ export const leftRotate = keyframes`
         transform: rotate(-360deg);
     }
 `;
+
+export const toDeepInsetShadow = keyframes`
+    from {
+        box-shadow: 0 0 0 0 rgba(0, 0, 0, 0.5) inset;
+    }
+
+    to {
+        box-shadow: 0 0 20px 0px rgba(0,0,0,0.5) inset;
+    }
+
+`;
