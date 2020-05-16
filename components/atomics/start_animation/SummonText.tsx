c0ba9841c7f11e67b86e8b71c8c7474f53935b2e
UMASHIBA <umashiba1@gmail.com>
2020-05-17 02:36:44 +0900

    [MOD]Summonを押したらアニメーションがかかるように変更

--- components/atomics/start_animation/SummonText.tsx
+++ components/atomics/start_animation/SummonText.tsx
@@ -1,22 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { ANGE_BROWN, ANGE_RED } from "../../../constants/colors";
 import { HWT_MARDELL_FONT_PROP } from "../../../constants/cssProps";
+import { toDeepInsetShadow } from "../../../styles/commonAnimation";
 
-const StyledSummonText = styled.div<{ diameter: number }>`
+const StyledSummonText = styled.div<{
+  diameter: number;
+  isStartAnimation: boolean;
+}>`
   ${HWT_MARDELL_FONT_PROP}
-  color: ${ANGE_RED};
-  position: absolute;
-  width: ${({ diameter }) => diameter}px;
-  height: ${({ diameter }) => diameter}px;
-  left: calc(50% - ${({ diameter }) => diameter / 2}px);
-  top: calc(50% - ${({ diameter }) => diameter / 2}px);
-  font-size: ${({ diameter }) => diameter / 7}px;
   display: flex;
   justify-content: center;
   align-items: center;
+  width: ${({ diameter }) => diameter}px;
+  height: ${({ diameter }) => diameter}px;
+  color: ${ANGE_RED};
   background-color ${ANGE_BROWN};
+  cursor: pointer;
   border-radius: 50%;
+  position: absolute;
+  left: calc(50% - ${({ diameter }) => diameter / 2}px);
+  top: calc(50% - ${({ diameter }) => diameter / 2}px);
+  font-size: ${({ diameter }) => diameter / 7}px;
+  animation: ${({ isStartAnimation }) =>
+    isStartAnimation ? toDeepInsetShadow : "none"} 300ms linear forwards 100ms;
   `;
 
 interface Props {
@@ -24,8 +31,16 @@ interface Props {
 }
 
 const SummonText: React.FC<Props> = ({ diameter: Diameter }: Props) => {
+  const [isStartAnimation, changeStartAnimation] = useState(false);
+  const startAnimation = () => {
+    changeStartAnimation(true);
+  };
   return (
-    <StyledSummonText diameter={Diameter}>
+    <StyledSummonText
+      diameter={Diameter}
+      isStartAnimation={isStartAnimation}
+      onClick={startAnimation}
+    >
       <span>Summon</span>
     </StyledSummonText>
   );
