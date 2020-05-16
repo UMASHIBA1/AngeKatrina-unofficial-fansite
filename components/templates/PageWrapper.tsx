9ab5926b4a6b2fb8dbe6c6de7b2fc4eb3092a5d9
UMASHIBA <umashiba1@gmail.com>
2020-05-16 23:53:22 +0900

    [MOD]PageWrapperのreduxのsizeの状態を変更する処理を変更

--- components/templates/PageWrapper.tsx
+++ components/templates/PageWrapper.tsx
@@ -1,8 +1,9 @@
 import React, { ReactNode } from "react";
-import { DispatchType } from "../../redux/store";
 import { useDispatch } from "react-redux";
 import { sm_breakpoint, tablet_breakpoint } from "../../constants/breakpoints";
-import { toSM, toTablet, toPC } from "../../redux/modules/sizes";
+import useDidMount from "../../hooks/useDidMount";
+import { toPC, toSM, toTablet } from "../../redux/modules/sizes";
+import { DispatchType } from "../../redux/store";
 
 interface Props {
   children: ReactNode;
@@ -11,13 +12,16 @@ interface Props {
 // 全てのページで必要な処理をまとめてあります。
 const PageWrapper: React.FC<Props> = (props: Props) => {
   const dispatch: DispatchType = useDispatch();
-  if (window.innerWidth < sm_breakpoint) {
-    dispatch(toSM());
-  } else if (window.innerWidth < tablet_breakpoint) {
-    dispatch(toTablet());
-  } else {
-    dispatch(toPC());
-  }
+
+  useDidMount(() => {
+    if (window.innerWidth < sm_breakpoint) {
+      dispatch(toSM());
+    } else if (window.innerWidth < tablet_breakpoint) {
+      dispatch(toTablet());
+    } else {
+      dispatch(toPC());
+    }
+  });
 
   return <React.Fragment>{props.children}</React.Fragment>;
 };
