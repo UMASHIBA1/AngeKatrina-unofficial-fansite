18df9834cee3cfaad73b1e402b0b42140168209b
UMASHIBA <umashiba1@gmail.com>
2020-05-18 14:09:05 +0900

    [MOD]ひとまず魔法陣が拡張されるアニメーションを追加

--- components/atomics/start_animation/SingleMagicCircle.tsx
+++ components/atomics/start_animation/SingleMagicCircle.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import styled, { Keyframes } from "styled-components";
+import styled, { Keyframes, keyframes } from "styled-components";
 import {
   rightRotate,
   leftRotate,
   toDeepDropShadow,
+  expand,
 } from "../../../styles/commonAnimation";
 import { ANGE_RED } from "../../../constants/colors";
 
@@ -14,7 +15,16 @@ interface Props {
   isStartSummonAnimation: boolean;
 }
 
-const SingleMagicCircle: React.FC<Props> = ({
+const changeTopLeft = (Diameter: Props["Diameter"]) => keyframes`
+  to {
+    transform: translate(
+      calc(50vw - ${Diameter / 2}px),
+      calc(50vh - ${Diameter / 2}px)
+    );
+  }
+`;
+
+const createAnimateStyledSVG = ({
   SvgElement,
   Diameter,
   rotateDirection,
@@ -29,13 +39,56 @@ const SingleMagicCircle: React.FC<Props> = ({
 
   const StyledSVG = styled(SvgElement)`
     position: absolute;
-    left: calc(50% - ${Diameter / 2}px);
-    top: calc(50% - ${Diameter / 2}px);
-    animation: ${rotateKeyframe} 20s linear infinite,
-      ${isStartSummonAnimation ? toDeepDropShadow(5, ANGE_RED) : "none"} 500ms
-        linear 400ms forwards;
   `;
 
-  return <StyledSVG width={Diameter} height={Diameter} />;
+  if (isStartSummonAnimation) {
+    return styled(StyledSVG)`
+      animation: ${rotateKeyframe} 20s linear infinite,
+        ${toDeepDropShadow(3, ANGE_RED)} 500ms linear 400ms forwards,
+        ${expand(Diameter * 1.2)} 500ms ease-in 900ms forwards;
+    `;
+  } else {
+    return styled(StyledSVG)`
+      animation: ${rotateKeyframe} 20s linear infinite;
+    `;
+  }
+};
+
+const createWrapper = (
+  isStartSummonAnimation: Props["isStartSummonAnimation"],
+  Diameter: Props["Diameter"]
+) => {
+  if (!isStartSummonAnimation) {
+    return styled.span<{ Diameter: number }>`
+      position: absolute;
+      transform: translate(
+        calc(50vw - ${({ Diameter }) => Diameter / 2}px),
+        calc(50vh - ${({ Diameter }) => Diameter / 2}px)
+      );
+    `;
+  } else {
+    return styled.span<{ Diameter: number }>`
+      position: absolute;
+      transform: translate(
+        calc(50vw - ${({ Diameter }) => Diameter / 2}px),
+        calc(50vh - ${({ Diameter }) => Diameter / 2}px)
+      );
+      animation: ${changeTopLeft(Diameter * 1.2)} 875ms ease-in 900ms forwards;
+    `;
+  }
+};
+
+const SingleMagicCircle: React.FC<Props> = (props: Props) => {
+  const { Diameter, isStartSummonAnimation } = props;
+
+  const AnimateStyledSVG = createAnimateStyledSVG(props);
+
+  const Wrapper = createWrapper(isStartSummonAnimation, Diameter);
+
+  return (
+    <Wrapper Diameter={Diameter}>
+      <AnimateStyledSVG width={Diameter} height={Diameter} />
+    </Wrapper>
+  );
 };
 export default SingleMagicCircle;
