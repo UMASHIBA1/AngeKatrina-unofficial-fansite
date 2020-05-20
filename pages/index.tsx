8fbe0c9f5455220b8ce6fa57e2814e8fc0e15c66
UMASHIBA <umashiba1@gmail.com>
2020-05-21 00:41:46 +0900

    [MOD]index.tsxにSmallMagicCircleMapを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,19 @@
 import styled from "styled-components";
 import MagicCircle from "../components/organisms/start_animation/MagicCircle";
 import PageWrapper from "../components/templates/PageWrapper";
+import SmallMagicCircleMap from "../components/organisms/start_animation/SmallMagicCircleMap";
 
 const Main = styled.main`
   width: 100%;
   height: 100%;
+  position: absolute;
 `;
 
 const IndexPage = () => (
   <PageWrapper>
     <Main>
       <MagicCircle />
+      <SmallMagicCircleMap />
     </Main>
   </PageWrapper>
 );
