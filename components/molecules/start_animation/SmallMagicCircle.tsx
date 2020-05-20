bc2b9f123ecffc917a9b0dcecf7faa9468727c6c
UMASHIBA <umashiba1@gmail.com>
2020-05-21 00:06:44 +0900

    [ADD]SmallMagicCircleを作成

---
+++ components/molecules/start_animation/SmallMagicCircle.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import styled from "styled-components";
+import MostIn from "../../../public/start_animation/svgs/most_in.svg";
+import SecondIn from "../../../public/start_animation/svgs/second_in.svg";
+import FourthIn from "../../../public/start_animation/svgs/fourth_in.svg";
+import SingleMagicCircle from "../../atomics/start_animation/SingleMagicCircle";
+
+interface Props {
+  top: number;
+  left: number;
+  diameter: number;
+  circleNum: 1 | 2 | 3;
+}
+
+const Wrapper = styled.div<{ left: number; top: number }>`
+  position: absolute;
+  top: ${({ top }) => top}px;
+  left: ${({ left }) => left}px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SmallMagicCircle: React.FC<Props> = ({
+  top,
+  left,
+  diameter,
+  circleNum,
+}: Props) => {
+  const mostInDiameter = diameter * 0.5;
+  const secondInDiameter = diameter * 0.78;
+
+  return (
+    <Wrapper top={top} left={left}>
+      {circleNum >= 3 ? (
+        <SingleMagicCircle
+          SvgElement={MostIn}
+          diameter={mostInDiameter}
+          rotateDirection={"right"}
+          isStartSummonAnimation={false}
+        />
+      ) : null}
+      {circleNum >= 2 ? (
+        <SingleMagicCircle
+          SvgElement={SecondIn}
+          diameter={secondInDiameter}
+          rotateDirection={"left"}
+          isStartSummonAnimation={false}
+        />
+      ) : null}
+      <SingleMagicCircle
+        SvgElement={FourthIn}
+        diameter={diameter}
+        rotateDirection={"right"}
+        isStartSummonAnimation={false}
+      />
+    </Wrapper>
+  );
+};
+
+export default SmallMagicCircle;
