4fc3bae46c7ed8d83d139392e9602b29c7d40b6e
UMASHIBA <umashiba1@gmail.com>
2020-05-22 02:25:43 +0900

    [MOD]isStartSummonAnimationを使いz-indexを操作するように変更

--- components/organisms/start_animation/BlackTransition.tsx
+++ components/organisms/start_animation/BlackTransition.tsx
@@ -2,19 +2,24 @@ import React from "react";
 import styled from "styled-components";
 import { blackTransitionZIndex } from "../../../constants/zindexs/start_animation";
 import BlackCircle from "../../atomics/start_animation/BlackCircle";
+import { useTypedSelector } from "../../../redux/store";
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{ isStartSummonAnimation: boolean }>`
   position: absolute;
   overflow: hidden;
   width: 100%;
   height: 100%;
-  z-index: ${blackTransitionZIndex};
+  z-index: ${({ isStartSummonAnimation }) =>
+    isStartSummonAnimation ? blackTransitionZIndex : 1};
 `;
 
 const BlackTransition: React.FC = () => {
+  const isStartSummonAnimation = useTypedSelector(
+    (state) => state.isStartSummonAnimation
+  );
   return (
-    <Wrapper>
-      <BlackCircle />
+    <Wrapper isStartSummonAnimation={isStartSummonAnimation}>
+      <BlackCircle isStartSummonAnimation={isStartSummonAnimation} />
     </Wrapper>
   );
 };
