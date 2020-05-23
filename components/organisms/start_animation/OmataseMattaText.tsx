2e8904ffcb83432c23ea485d23c0cc564d419382
UMASHIBA <umashiba1@gmail.com>
2020-05-24 01:18:57 +0900

    [MOD]OmataseMattaTextのfont-sizeを指定

--- components/organisms/start_animation/OmataseMattaText.tsx
+++ components/organisms/start_animation/OmataseMattaText.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { SizeType } from "../../../typing/SizeType";
 import { useTypedSelector } from "../../../redux/store";
 import { omataseMattaTextZIndex } from "../../../constants/start_animation/zindex";
+import sizeTypeJudge from "../../../systems/sizeTypeJudge";
 
 const calcFlexDirection = (size: SizeType) => {
   if (size === "sm") {
@@ -21,14 +22,14 @@ const Wrapper = styled.ul<{ size: SizeType }>`
   z-index: ${omataseMattaTextZIndex};
 `;
 
-const CharWrapper = styled.li`
+const CharWrapper = styled.li<{ size: SizeType }>`
   display: flex;
   align-items: center;
   justify-content: center;
   font-family: kan412typos-std, sans-serif;
   font-style: normal;
   font-weight: 400;
-  font-size: 
+  font-size: ${({ size }) => sizeTypeJudge(size)("2.3rem", "3.3rem", "4.3rem")};
   color: #ffffff;
 `;
 
@@ -39,7 +40,9 @@ const OmataseMattaText = () => {
   return (
     <Wrapper size={size}>
       {omataseMatta.split("").map((value, index) => (
-        <CharWrapper key={index}>{value}</CharWrapper>
+        <CharWrapper key={index} size={size}>
+          {value}
+        </CharWrapper>
       ))}
     </Wrapper>
   );
