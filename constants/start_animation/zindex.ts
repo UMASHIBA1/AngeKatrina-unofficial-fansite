1a04c3cd0f00a7ceef135d45971ad7f6271ecd94
UMASHIBA <umashiba1@gmail.com>
2020-05-24 00:21:19 +0900

    [MOD]omataseMattaZIndexを追加、AngeTriangleのzindexを変更

--- constants/start_animation/zindex.ts
+++ constants/start_animation/zindex.ts
@@ -1,4 +1,5 @@
 export const smallMagicCircleMapZIndex = 100;
 export const magicCircleZIndex = 101;
 export const blackTransitionZIndex = 102;
-export const angeTriangleZIndex = 103;
+export const omataseMattaTextZIndex = 103;
+export const angeTriangleZIndex = 104;
