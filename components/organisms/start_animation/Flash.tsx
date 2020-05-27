853fe90353db763323a0a2ff633062b6f1bdd96b
UMASHIBA <umashiba1@gmail.com>
2020-05-28 03:25:05 +0900

    [MOD]Flashアニメーションの光源のような表現を作成

--- components/organisms/start_animation/Flash.tsx
+++ components/organisms/start_animation/Flash.tsx
@@ -5,22 +5,59 @@ import { flashOrder } from "../../../constants/start_animation/animation_order";
 import { flashZIndex } from "../../../constants/start_animation/zindex";
 import { useTypedSelector } from "../../../redux/store";
 
-const FlashMain = styled.div<{ isStartAnimation: boolean }>`
+const Wrapper = styled.div<{ isStartAnimation: boolean }>`
   position: absolute;
   z-index: ${({ isStartAnimation }) => (isStartAnimation ? flashZIndex : "0")};
   width: 100%;
   height: 100%;
-  opacity: 0;
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+  overflow: hidden;
+`;
+
+const FlashMain = styled.div<{
+  isStartAnimation: boolean;
+  diameter: string;
+  blur: string;
+}>`
+  position: absolute;
+  width: ${({ diameter }) => diameter};
+  height: ${({ diameter }) => diameter};
+  border-radius: 50%;
+  transform: translate(calc(${({ diameter }) => diameter} / 2), calc(${({
+  diameter,
+}) => diameter} / 2));
+  filter: blur(${({ blur }) => blur});
   background-color: #ffffff;
-  animation: ${({ isStartAnimation }) => (isStartAnimation ? fadein() : "none")}
-    ${flashOrder.duration_ms}ms ease-out ${flashOrder.delay_ms}ms forwards;
+//   animation: ${({ isStartAnimation }) =>
+  isStartAnimation ? fadein() : "none"}
+//     ${flashOrder.duration_ms}ms ease-out ${flashOrder.delay_ms}ms forwards;
 `;
 
 const Flash: React.FC = () => {
   const isStartAnimation = useTypedSelector(
     (state) => state.isStartSummonAnimation
   );
-  return <FlashMain isStartAnimation={isStartAnimation} />;
+  return (
+    <Wrapper isStartAnimation={isStartAnimation}>
+      <FlashMain
+        isStartAnimation={isStartAnimation}
+        diameter="200px"
+        blur="100px"
+      />
+      <FlashMain
+        isStartAnimation={isStartAnimation}
+        diameter="200px"
+        blur="50px"
+      />
+      <FlashMain
+        isStartAnimation={isStartAnimation}
+        diameter="200px"
+        blur="25px"
+      />
+    </Wrapper>
+  );
 };
 
 export default Flash;
