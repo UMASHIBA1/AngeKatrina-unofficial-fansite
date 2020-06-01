b068715b46573db7de5350bf3736868aea4dcc50
UMASHIBA <umashiba1@gmail.com>
2020-06-02 03:16:45 +0900

    [ADD]Flashの新しい原型を作成

--- components/organisms/start_animation/Flash.tsx
+++ components/organisms/start_animation/Flash.tsx
@@ -4,6 +4,8 @@ import { fadein } from "../../../styles/commonAnimation";
 import { flashOrder } from "../../../constants/start_animation/animation_order";
 import { flashZIndex } from "../../../constants/start_animation/zindex";
 import { useTypedSelector } from "../../../redux/store";
+import FlashLineSVG from '../../../public/start_animation/svgs/flash_line.svg';
+import WhiteCircle from '../../../public/start_animation/svgs/white_circle.svg';
 
 const Wrapper = styled.div<{ isStartAnimation: boolean }>`
   position: absolute;
@@ -12,28 +14,48 @@ const Wrapper = styled.div<{ isStartAnimation: boolean }>`
   height: 100%;
   display: flex;
   justify-content: flex-end;
-  align-items: flex-end;
+  align-items: center;
   overflow: hidden;
 `;
 
-const FlashMain = styled.div<{
+interface SVGProps {
   isStartAnimation: boolean;
   diameter: string;
   blur: string;
-}>`
+}
+
+const StarSVG: React.FC<SVGProps> = ({
+  isStartAnimation, diameter, blur, ...props
+}: SVGProps) => {
+  return (
+    <FlashLineSVG {...props} />
+  );
+}
+
+type LineFlashProps = SVGProps & {rotate?: string, top?: string}
+
+
+const LineFlash = styled(StarSVG)<LineFlashProps>`
   position: absolute;
   width: ${({ diameter }) => diameter};
   height: ${({ diameter }) => diameter};
-  border-radius: 50%;
-  transform: translate(calc(${({ diameter }) => diameter} / 2), calc(${({
-  diameter,
-}) => diameter} / 2));
+  transform: translate(calc(${({ diameter }) => diameter} / 2), ${({top}) => top!==undefined?top: "0"}) rotate(${({rotate}) => rotate!==undefined?rotate:"0deg"});
   filter: blur(${({ blur }) => blur});
-  background-color: #ffffff;
 //   animation: ${({ isStartAnimation }) =>
   isStartAnimation ? fadein() : "none"}
 //     ${flashOrder.duration_ms}ms ease-out ${flashOrder.delay_ms}ms forwards;
 `;
+
+const CircleFlash = styled(WhiteCircle)<SVGProps>`
+  position: absolute;
+  width: ${({ diameter }) => diameter};
+  height: ${({ diameter }) => diameter};
+  transform: translate(calc(${({ diameter }) => diameter} / 2 + ${({diameter}) => diameter} / 6), 0) rotate(0deg);
+  filter: blur(${({ blur }) => blur});
+  //   animation: ${({ isStartAnimation }) =>
+  isStartAnimation ? fadein() : "none"}
+  //     ${flashOrder.duration_ms}ms ease-out ${flashOrder.delay_ms}ms forwards;
+`
 
 const Flash: React.FC = () => {
   const isStartAnimation = useTypedSelector(
@@ -41,20 +63,39 @@ const Flash: React.FC = () => {
   );
   return (
     <Wrapper isStartAnimation={isStartAnimation}>
-      <FlashMain
+      <LineFlash
         isStartAnimation={isStartAnimation}
-        diameter="200px"
-        blur="100px"
+        diameter="1000px"
+        blur="10px"
       />
-      <FlashMain
+      <LineFlash
+        isStartAnimation={isStartAnimation}
+        diameter="800px"
+        blur="15px"
+        rotate="40deg"
+        top="-70px"
+      />
+      <LineFlash
+        isStartAnimation={isStartAnimation}
+        diameter="550px"
+        blur="15px"
+        rotate="-60deg"
+        top="60px"
+      />
+      <CircleFlash 
+        isStartAnimation={isStartAnimation}
+        diameter="300px"
+        blur="60px"
+      />
+      <CircleFlash 
+        isStartAnimation={isStartAnimation}
+        diameter="300px"
+        blur="300px"
+      />
+      <CircleFlash 
         isStartAnimation={isStartAnimation}
         diameter="200px"
         blur="50px"
-      />
-      <FlashMain
-        isStartAnimation={isStartAnimation}
-        diameter="200px"
-        blur="25px"
       />
     </Wrapper>
   );
