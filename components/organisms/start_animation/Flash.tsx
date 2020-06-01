7b26d3a7885638bfc9a32d10e31de03680619466
UMASHIBA <umashiba1@gmail.com>
2020-06-02 05:36:09 +0900

    [MOD]Flashのコードをフォーマット

--- components/organisms/start_animation/Flash.tsx
+++ components/organisms/start_animation/Flash.tsx
@@ -4,8 +4,8 @@ import { fadein } from "../../../styles/commonAnimation";
 import { flashOrder } from "../../../constants/start_animation/animation_order";
 import { flashZIndex } from "../../../constants/start_animation/zindex";
 import { useTypedSelector } from "../../../redux/store";
-import FlashLineSVG from '../../../public/start_animation/svgs/flash_line.svg';
-import WhiteCircleSVG from '../../../public/start_animation/svgs/white_circle.svg';
+import FlashLineSVG from "../../../public/start_animation/svgs/flash_line.svg";
+import WhiteCircleSVG from "../../../public/start_animation/svgs/white_circle.svg";
 
 const Wrapper = styled.div<{ isStartAnimation: boolean }>`
   position: absolute;
@@ -24,23 +24,33 @@ interface SVGProps {
   blur: string;
 }
 
-const WhiteCircle: React.FC<SVGProps> = ({isStartAnimation, blur, scale, ...props}:SVGProps) => {
-  return (
-    <WhiteCircleSVG {...props} />
-  )
-}
+const WhiteCircle: React.FC<SVGProps> = ({
+  isStartAnimation,
+  blur,
+  scale,
+  ...props
+}: SVGProps) => {
+  return <WhiteCircleSVG {...props} />;
+};
 
-type LineFlashProps = SVGProps & {rotate?: string, top?: string}
+type LineFlashProps = SVGProps & { rotate?: string; top?: string };
 
 const FlashLine: React.FC<LineFlashProps> = ({
-  isStartAnimation, blur, rotate, top, scale, ...props
+  isStartAnimation,
+  blur,
+  rotate,
+  top,
+  scale,
+  ...props
 }: LineFlashProps) => {
-  return (
-    <FlashLineSVG {...props} />
-  );
-}
+  return <FlashLineSVG {...props} />;
+};
 
-const lineFlashAnimation = ({top = "0", scale, rotate="0deg"}: Pick<LineFlashProps, "top" | "scale" | "rotate">) => keyframes`
+const lineFlashAnimation = ({
+  top = "0",
+  scale,
+  rotate = "0deg",
+}: Pick<LineFlashProps, "top" | "scale" | "rotate">) => keyframes`
   from {
     transform: translate(calc(100px / 2), ${top}) scale(0.0001) rotate(${rotate});
   }
@@ -58,16 +68,25 @@ const circleFlashAnimation = (scale: LineFlashProps["scale"]) => keyframes`
   to {
     transform: translate(calc(100px / 2), 0) scale(${scale}) rotate(0deg);
   }
-`
+`;
 
 const LineFlash = styled(FlashLine)<LineFlashProps>`
   position: absolute;
   width: 100px;
   height: 100px;
-  transform: translate(calc(100px / 2), ${({top}) => top!==undefined?top: "0"}) scale(${({scale})=>scale}) rotate(${({rotate}) => rotate!==undefined?rotate:"0deg"});
+  transform: translate(
+      calc(100px / 2),
+      ${({ top }) => (top !== undefined ? top : "0")}
+    )
+    scale(${({ scale }) => scale})
+    rotate(${({ rotate }) => (rotate !== undefined ? rotate : "0deg")});
   filter: blur(${({ blur }) => blur});
   opacity: 0;
-  animation: ${({ isStartAnimation }) => isStartAnimation ? fadein() : "none"} 0ms ease-out ${flashOrder.delay_ms}ms forwards, ${({isStartAnimation,scale, rotate, top })=>isStartAnimation?lineFlashAnimation({scale, rotate, top}): "none"} ${flashOrder.duration_ms}ms ease-out ${flashOrder.delay_ms}ms both;
+  animation: ${({ isStartAnimation }) => (isStartAnimation ? fadein() : "none")}
+      0ms ease-out ${flashOrder.delay_ms}ms forwards,
+    ${({ isStartAnimation, scale, rotate, top }) =>
+        isStartAnimation ? lineFlashAnimation({ scale, rotate, top }) : "none"}
+      ${flashOrder.duration_ms}ms ease-out ${flashOrder.delay_ms}ms both;
 `;
 
 const CircleFlash = styled(WhiteCircle)<SVGProps>`
@@ -77,8 +96,12 @@ const CircleFlash = styled(WhiteCircle)<SVGProps>`
 
   filter: blur(${({ blur }) => blur});
   opacity: 0;
-  animation: ${({ isStartAnimation }) => isStartAnimation ? fadein() : "none"} 0ms ease-out ${flashOrder.delay_ms}ms forwards, ${({isStartAnimation,scale}) => isStartAnimation?circleFlashAnimation(scale): "none"} ${flashOrder.duration_ms}ms ease-out ${flashOrder.delay_ms}ms both;
-`
+  animation: ${({ isStartAnimation }) => (isStartAnimation ? fadein() : "none")}
+      0ms ease-out ${flashOrder.delay_ms}ms forwards,
+    ${({ isStartAnimation, scale }) =>
+        isStartAnimation ? circleFlashAnimation(scale) : "none"}
+      ${flashOrder.duration_ms}ms ease-out ${flashOrder.delay_ms}ms both;
+`;
 
 const Flash: React.FC = () => {
   const isStartAnimation = useTypedSelector(
@@ -86,11 +109,7 @@ const Flash: React.FC = () => {
   );
   return (
     <Wrapper isStartAnimation={isStartAnimation}>
-      <LineFlash
-        isStartAnimation={isStartAnimation}
-        scale={10}
-        blur="1px"
-      />
+      <LineFlash isStartAnimation={isStartAnimation} scale={10} blur="1px" />
       <LineFlash
         isStartAnimation={isStartAnimation}
         scale={8}
@@ -105,21 +124,9 @@ const Flash: React.FC = () => {
         rotate="-60deg"
         top="60px"
       />
-      <CircleFlash 
-        isStartAnimation={isStartAnimation}
-        scale={3}
-        blur="20px"
-      />
-      <CircleFlash 
-        isStartAnimation={isStartAnimation}
-        scale={3}
-        blur="100px"
-      />
-      <CircleFlash 
-        isStartAnimation={isStartAnimation}
-        scale={2}
-        blur="25px"
-      />
+      <CircleFlash isStartAnimation={isStartAnimation} scale={3} blur="20px" />
+      <CircleFlash isStartAnimation={isStartAnimation} scale={3} blur="100px" />
+      <CircleFlash isStartAnimation={isStartAnimation} scale={2} blur="25px" />
     </Wrapper>
   );
 };
