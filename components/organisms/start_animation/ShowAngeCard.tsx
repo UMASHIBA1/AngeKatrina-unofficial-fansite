b44a3de37316de63587d693a63095b2a6e51749c
UMASHIBA <umashiba1@gmail.com>
2020-06-06 01:19:22 +0900

    [MOD]新しいshowAngeCardのアニメーションの原型を作成

--- components/organisms/start_animation/ShowAngeCard.tsx
+++ components/organisms/start_animation/ShowAngeCard.tsx
@@ -1,11 +1,31 @@
 import React from "react";
-// import styled from "styled-components";
-// import { showAngeCardOrder } from "../../../constants/start_animation/animation_order";
-// import { useTypedSelector } from "../../../redux/store";
-// import { showAngeCardZIndex } from "../../../constants/start_animation/zindex";
+import styled from "styled-components";
+import { showAngeCardZIndex } from "../../../constants/start_animation/zindex";
+import { useTypedSelector } from "../../../redux/store";
+import CircleFlash from "../../molecules/start_animation/CircleFlash";
+
+const Wrapper = styled.div<{ isStartAnimation: boolean }>`
+  position: absolute;
+  z-index: ${({ isStartAnimation }) =>
+    isStartAnimation ? showAngeCardZIndex : "0"};
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-item: center;
+  overflow: hidden;
+`;
 
 const ShowAngeCard: React.FC = () => {
-  return <></>;
+  const isStartAnimation = useTypedSelector(
+    (state) => state.isStartSummonAnimation
+  );
+
+  return (
+    <Wrapper isStartAnimation={isStartAnimation}>
+      <CircleFlash isStartAnimation={isStartAnimation} scale={3} blur="100px" />
+    </Wrapper>
+  );
 };
 
 export default ShowAngeCard;
