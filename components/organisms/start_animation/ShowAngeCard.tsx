98f63b9499dec607294203ad5ee8aa367d117d17
UMASHIBA <umashiba1@gmail.com>
2020-06-07 02:32:16 +0900

    [MOD]右下から左上にだんだん薄くしていくアニメーションの原型を作成

--- components/organisms/start_animation/ShowAngeCard.tsx
+++ components/organisms/start_animation/ShowAngeCard.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import { showAngeCardZIndex } from "../../../constants/start_animation/zindex";
 import { useTypedSelector } from "../../../redux/store";
+import { fadeout, fadein } from "../../../styles/commonAnimation";
+import { showAngeCardOrder } from "../../../constants/start_animation/animation_order";
 
 const Wrapper = styled.div<{ isStartAnimation: boolean }>`
   position: absolute;
@@ -10,15 +12,49 @@ const Wrapper = styled.div<{ isStartAnimation: boolean }>`
   width: 100%;
   height: 100%;
   display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+  justify-content: center;
   overflow: hidden;
 `;
 
+const Line = styled.div<{
+  isStartAnimation: boolean;
+  width: string;
+  height: string;
+  additionalDelay: number;
+}>`
+  display: flex;
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+  background-color: #ffffff;
+  animation: ${({ isStartAnimation }) => (isStartAnimation ? fadein() : "none")}
+      0ms linear ${showAngeCardOrder.delay_ms}ms both,
+    ${({ isStartAnimation }) => (isStartAnimation ? fadeout : "none")}
+      ${showAngeCardOrder.duration_ms}ms linear
+      ${({ additionalDelay }) => showAngeCardOrder.delay_ms + additionalDelay}ms
+      forwards;
+`;
+
 const ShowAngeCard: React.FC = () => {
-  const isStartAnimation = useTypedSelector((state) => [
-    state.isStartSummonAnimation,
-  ]);
+  const isStartAnimation = useTypedSelector(
+    (state) => state.isStartSummonAnimation
+  );
+
+  const lineNum = 10;
 
   return (
+    <Wrapper isStartAnimation={isStartAnimation}>
+      {[...Array(lineNum).keys()].map((index) => (
+        <Line
+          isStartAnimation={isStartAnimation}
+          key={index}
+          width={`calc(100% / ${lineNum})`}
+          height={`100%`}
+          additionalDelay={20 * index}
+        />
+      ))}
+    </Wrapper>
   );
 };
 
