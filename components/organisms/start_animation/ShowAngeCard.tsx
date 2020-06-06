37e63b0a10e994622e0ca32ea9d067fc84e2ea57
UMASHIBA <umashiba1@gmail.com>
2020-06-06 22:59:00 +0900

    [DEL]blurした円を小さくする方式のアニメーションを削除

--- components/organisms/start_animation/ShowAngeCard.tsx
+++ components/organisms/start_animation/ShowAngeCard.tsx
@@ -2,11 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import { showAngeCardZIndex } from "../../../constants/start_animation/zindex";
 import { useTypedSelector } from "../../../redux/store";
-import ShowScreenCircleMain from "../../molecules/start_animation/ShowScreenCircle";
-import {
-  sm_breakpoint,
-  tablet_breakpoint,
-} from "../../../constants/breakpoints";
 
 const Wrapper = styled.div<{ isStartAnimation: boolean }>`
   position: absolute;
@@ -15,48 +10,15 @@ const Wrapper = styled.div<{ isStartAnimation: boolean }>`
   width: 100%;
   height: 100%;
   display: flex;
-  justify-content: flex-start;
-  align-items: flex-start;
   overflow: hidden;
 `;
 
 const ShowAngeCard: React.FC = () => {
-  const [isStartAnimation, size] = useTypedSelector((state) => [
+  const isStartAnimation = useTypedSelector((state) => [
     state.isStartSummonAnimation,
-    state.sizes,
   ]);
 
-  let showScreenCircleInitialScale;
-  if (size === "sm") {
-    showScreenCircleInitialScale = (sm_breakpoint / 100) * 2;
-  } else if (size === "tablet") {
-    showScreenCircleInitialScale = (tablet_breakpoint / 100) * 2;
-  } else {
-    // NOTE 画面の対応サイズは1920pxまでなので1920px / 100px * 2
-    showScreenCircleInitialScale = 38.4;
-  }
-
   return (
-    <Wrapper isStartAnimation={isStartAnimation}>
-      <ShowScreenCircleMain
-        isStartAnimation={isStartAnimation}
-        scale={showScreenCircleInitialScale}
-        blur="6px"
-        opacity={0.6}
-      />
-      <ShowScreenCircleMain
-        isStartAnimation={isStartAnimation}
-        scale={showScreenCircleInitialScale}
-        blur="20px"
-        opacity={1}
-      />
-      <ShowScreenCircleMain
-        isStartAnimation={isStartAnimation}
-        scale={showScreenCircleInitialScale}
-        blur="40px"
-        opacity={0.6}
-      />
-    </Wrapper>
   );
 };
 
