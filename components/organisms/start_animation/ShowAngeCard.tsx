d5ba14fdc52cdfe70b017098dc356a964360150f
UMASHIBA <umashiba1@gmail.com>
2020-06-08 00:13:14 +0900

    [MOD]右下から左上にだんだん薄くしていくアニメーションを作成

--- components/organisms/start_animation/ShowAngeCard.tsx
+++ components/organisms/start_animation/ShowAngeCard.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { showAngeCardZIndex } from "../../../constants/start_animation/zindex";
 import { useTypedSelector } from "../../../redux/store";
 import { fadeout, fadein } from "../../../styles/commonAnimation";
 import { showAngeCardOrder } from "../../../constants/start_animation/animation_order";
+import useDidMount from "../../../hooks/useDidMount";
 
 const Wrapper = styled.div<{ isStartAnimation: boolean }>`
   position: absolute;
@@ -11,11 +12,27 @@ const Wrapper = styled.div<{ isStartAnimation: boolean }>`
     isStartAnimation ? showAngeCardZIndex : "0"};
   width: 100%;
   height: 100%;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const calcDiagonalLine = (width: number, height: number) => {
+  return Math.sqrt(width ** 2 + height ** 2);
+};
+
+const calcLineWidth = (lineNum: number, width: number, height: number) => {
+  const diagonalLine = calcDiagonalLine(width, height);
+  return diagonalLine / lineNum;
+};
+
+const LineWrapper = styled.div`
   display: flex;
   flex-direction: row-reverse;
   align-items: center;
   justify-content: center;
-  overflow: hidden;
+  transform: rotate(45deg);
 `;
 
 const Line = styled.div<{
@@ -24,10 +41,10 @@ const Line = styled.div<{
   height: string;
   additionalDelay: number;
 }>`
-  display: flex;
   width: ${({ width }) => width};
   height: ${({ height }) => height};
   background-color: #ffffff;
+  margin: 0;
   animation: ${({ isStartAnimation }) => (isStartAnimation ? fadein() : "none")}
       0ms linear ${showAngeCardOrder.delay_ms}ms both,
     ${({ isStartAnimation }) => (isStartAnimation ? fadeout : "none")}
@@ -36,24 +53,39 @@ const Line = styled.div<{
       forwards;
 `;
 
+const useWindowWidthHeight = () => {
+  const [width, changeWidth] = useState(0);
+  const [height, changeHeight] = useState(0);
+  useDidMount(() => {
+    changeWidth(window.innerWidth);
+    changeHeight(window.innerHeight);
+  });
+  return [width, height];
+};
+
 const ShowAngeCard: React.FC = () => {
   const isStartAnimation = useTypedSelector(
     (state) => state.isStartSummonAnimation
   );
 
-  const lineNum = 10;
+  const lineNum = 15;
+  const [windowWidth, windowHeight] = useWindowWidthHeight();
+  const lineWidth = `${calcLineWidth(lineNum, windowWidth, windowHeight)}px`;
+  const lineHeight = `${calcDiagonalLine(windowWidth, windowHeight)}px`;
 
   return (
     <Wrapper isStartAnimation={isStartAnimation}>
-      {[...Array(lineNum).keys()].map((index) => (
-        <Line
-          isStartAnimation={isStartAnimation}
-          key={index}
-          width={`calc(100% / ${lineNum})`}
-          height={`100%`}
-          additionalDelay={20 * index}
-        />
-      ))}
+      <LineWrapper>
+        {[...Array(lineNum).keys()].map((index) => (
+          <Line
+            isStartAnimation={isStartAnimation}
+            key={index}
+            width={lineWidth}
+            height={lineHeight}
+            additionalDelay={20 * index}
+          />
+        ))}
+      </LineWrapper>
     </Wrapper>
   );
 };
