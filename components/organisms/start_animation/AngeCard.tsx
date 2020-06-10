2d21b5f03fa51ad1a66799902c4edf7e9c88e95d
UMASHIBA <umashiba1@gmail.com>
2020-06-11 01:03:20 +0900

    [ADD]AngeCardのすべての画面サイズで適用される枠組みのデザインを作成

---
+++ components/organisms/start_animation/AngeCard.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+import { ANGE_LIVE_BACK_COLOR } from "../../../constants/colors";
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  background-color: #ffffff;
+`;
+
+const BackGround = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: calc(100% - 10px);
+  height: calc(100% - 10px);
+  background-color: ${ANGE_LIVE_BACK_COLOR};
+  border-radius: 20px;
+`;
+
+const WhiteBorderBG = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: calc(100% - 10px);
+  height: calc(100% - 10px);
+  background-color: ${ANGE_LIVE_BACK_COLOR};
+  border-radius: 14px;
+  border: #ffffff solid 5px;
+`;
+
+const AngeCard: React.FC = () => {
+  return (
+    <Wrapper>
+      <BackGround>
+        <WhiteBorderBG />
+      </BackGround>
+    </Wrapper>
+  );
+};
+
+export default AngeCard;
