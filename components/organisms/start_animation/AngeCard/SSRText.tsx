317c76ad66cbab8fd6772387d5cade9807f38127
UMASHIBA <umashiba1@gmail.com>
2020-06-12 04:41:21 +0900

    [MOD]tablet,pc用のSSRTextを作成

--- components/organisms/start_animation/AngeCard/SSRText.tsx
+++ components/organisms/start_animation/AngeCard/SSRText.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import styled from "styled-components";
 import { ANGE_LIVE_BACK_COLOR } from "../../../../constants/colors";
 import { BUNKYU_MIDASHI_GO_STD } from "../../../../constants/cssProps";
+import {
+  sm_breakpoint,
+  tablet_breakpoint,
+} from "../../../../constants/breakpoints";
 
 const Wrapper = styled.div`
   display: flex;
@@ -16,12 +20,26 @@ const TextBG = styled.div`
   justify-content: center;
   background-color: #ffffff;
   color: ${ANGE_LIVE_BACK_COLOR};
-  width: 80px;
-  height: 40px;
-  border-radius: 0 4px 0 14px;
+  border-radius: 0 0px 0 14px;
   border: #ffffff solid 5px;
-  ${BUNKYU_MIDASHI_GO_STD}
-  font-size: 1.5rem;
+  ${BUNKYU_MIDASHI_GO_STD};
+  @media (max-width: ${sm_breakpoint}px) {
+    width: 80px;
+    height: 40px;
+    font-size: 1.5rem;
+  }
+
+  @media (min-width: ${sm_breakpoint + 1}px) {
+    width: 110px;
+    height: 50px;
+    font-size: 1.7rem;
+  }
+
+  @media (min-width: ${tablet_breakpoint + 1}px) {
+    width: 140px;
+    height: 60px;
+    font-size: 2.5rem;
+  }
 `;
 
 const SSRText: React.FC = () => {
