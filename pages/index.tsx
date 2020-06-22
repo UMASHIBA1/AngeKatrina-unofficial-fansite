0682f975c1007c3fa331b4d3ac60982ad44c5013
UMASHIBA <umashiba1@gmail.com>
2020-06-23 00:52:23 +0900

    [MOD]indexを真っ白に変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,7 @@
-import styled from "styled-components";
-import MagicCircle from "../components/organisms/start_animation/MagicCircle/MagicCircle";
-import PageWrapper from "../components/templates/PageWrapper";
-import SmallMagicCircleMap from "../components/organisms/start_animation/SmallMagicCircleMap/SmallMagicCircleMap";
-import { ANGE_WHITE } from "../constants/colors";
-import BlackTransition from "../components/organisms/start_animation/BlackTransition/BlackTransition";
-import OmataseMattaText from "../components/organisms/start_animation/OmataseMattaText";
-import AngeTriangle from "../components/organisms/start_animation/AngeTriangle";
-import Flash from "../components/organisms/start_animation/Flash/Flash";
-import ShowAngeCard from "../components/organisms/start_animation/ShowAngeCard";
-import { GetServerSideProps } from "next";
-import getRandomInt from "../systems/getRandomInt";
-import AngeCard from "../components/organisms/start_animation/AngeCard/AngeCard";
+import React from "react";
 
-const Main = styled.main`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  background-color: ${ANGE_WHITE};
-`;
-
-interface Props {
-  randomInt: number;
-}
-
-// NOTE ランダム値をサーバー側で生成してAngeImgの表示される画像をサーバー、クライアント同じにしてる
-export const getServerSideProps: GetServerSideProps<Props> = async () => {
-  return {
-    props: {
-      randomInt: getRandomInt(3),
-    },
-  };
+const Home: React.FC = () => {
+  return <div></div>;
 };
 
-const IndexPage = ({ randomInt }: Props) => (
-  <PageWrapper>
-    <Main>
-      <MagicCircle />
-      <SmallMagicCircleMap />
-      <BlackTransition />
-      <AngeTriangle />
-      <OmataseMattaText />
-      <Flash />
-      <ShowAngeCard />
-      <AngeCard randomInt={randomInt} />
-    </Main>
-  </PageWrapper>
-);
-
-export default IndexPage;
+export default Home;
