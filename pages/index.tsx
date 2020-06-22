1fab88da508e6050e4cc65425c956f5e9a2fd077
UMASHIBA <umashiba1@gmail.com>
2020-06-23 01:15:47 +0900

    [MOD]Homeの背景作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+import { ANGE_LIVE_BACK_COLOR } from "../constants/colors";
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  position: absolute;
+  background-color: ${ANGE_LIVE_BACK_COLOR};
+`;
 
 const Home: React.FC = () => {
-  return <div></div>;
+  return (
+    <Wrapper>
+      {/* aaaaa */}
+      {/* dad */}
+    </Wrapper>
+  );
 };
 
 export default Home;
