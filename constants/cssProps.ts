dd4659d42d99e2763a35fc9458f67c3c3aeb4b3d
UMASHIBA <umashiba1@gmail.com>
2020-06-23 23:27:19 +0900

    [MOD]TA-F1ブロックラインフォントのcss用定数を作成

--- constants/cssProps.ts
+++ constants/cssProps.ts
@@ -8,3 +8,9 @@ export const BUNKYU_MIDASHI_GO_STD = `
 font-family: toppan-bunkyu-midashi-go-std, sans-serif;
 font-weight: 900;
 font-style: normal;`;
+
+export const TA_F1_BLOCK_LINE = `
+font-family: ta-f1blockline,sans-serif;
+font-weight: 400;
+font-style: normal;
+`;
