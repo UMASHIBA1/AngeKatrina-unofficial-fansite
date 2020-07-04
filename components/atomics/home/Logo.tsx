8a56af3394bc548f7da42daea55bb41e93e34341
UMASHIBA <umashiba1@gmail.com>
2020-07-04 23:19:54 +0900

    [MOD]画面幅によりLogoの大きさを変えるように変更

--- components/atomics/home/Logo.tsx
+++ components/atomics/home/Logo.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import LogoImg from "../../../public/svgs/home/logo.svg";
 import styled from "styled-components";
+import { sm_breakpoint } from "../../../constants/breakpoints";
 
 const Wrapper = styled.span`
-  width: 300px;
+  width: 150px;
   margin: 10px 0 0 10px;
+  @media (min-width: ${sm_breakpoint}px) {
+    width: 300px;
+  }
 `;
 
 const Logo: React.FC = () => {
