3d21c91f132c8377b1c8acf7792501963208592a
UMASHIBA <umashiba1@gmail.com>
2020-07-05 01:20:10 +0900

    [MOD]HomeBG作成完了

--- components/organisms/home/HomeBG.tsx
+++ components/organisms/home/HomeBG.tsx
@@ -8,6 +8,8 @@ import {
 import BGTriangle, {
   Props as BGTriangleProp,
 } from "../../atomics/home/BGTriangle";
+import { useTypedSelector } from "../../../redux/store";
+import sizeTypeJudge from "../../../systems/sizeTypeJudge";
 
 interface Props {
   children: ReactNode;
@@ -40,13 +42,154 @@ const ContentWrapper = styled.div`
   z-index: ${homeContentZIndex};
 `;
 
+const smBGTriangleProps: BGTriangleProp[] = [
+  {
+    color: "yellow",
+    width: "80px",
+    top: "120px",
+    left: "26px",
+  },
+  {
+    color: "white",
+    width: "70px",
+    rotate: "40deg",
+    top: "140px",
+    right: "40px",
+  },
+  {
+    color: "ange",
+    width: "90px",
+    rotate: "40deg",
+    bottom: "60px",
+    left: "40px",
+  },
+  {
+    color: "yellow",
+    width: "45px",
+    rotate: "-40deg",
+    bottom: "160px",
+    right: "40px",
+  },
+];
+
+const tabletBGTriangleProps: BGTriangleProp[] = [
+  {
+    color: "white",
+    width: "140px",
+    rotate: "40deg",
+    top: "130px",
+    left: "30px",
+  },
+  {
+    color: "yellow",
+    width: "110px",
+    rotate: "20deg",
+    top: "200px",
+    right: "40px",
+  },
+  {
+    color: "ange",
+    width: "160px",
+    rotate: "40deg",
+    bottom: "150px",
+    left: "40px",
+  },
+  {
+    color: "white",
+    width: "70px",
+    rotate: "45deg",
+    bottom: "300px",
+    right: "10px",
+  },
+  {
+    color: "yellow",
+    width: "120px",
+    rotate: "-20deg",
+    bottom: "10px",
+    right: "260px",
+  },
+];
+
+const pcBGTriangleProps: BGTriangleProp[] = [
+  {
+    color: "ange",
+    width: "150px",
+    rotate: "40deg",
+    top: "140px",
+    left: "50px",
+  },
+  {
+    color: "yellow",
+    width: "40px",
+    rotate: "-20deg",
+    top: "100px",
+    left: "40%",
+  },
+  {
+    color: "yellow",
+    width: "60px",
+    rotate: "35deg",
+    top: "90px",
+    right: "28%",
+  },
+  {
+    color: "white",
+    width: "40px",
+    rotate: "15deg",
+    top: "200px",
+    right: "70px",
+  },
+  {
+    color: "white",
+    width: "180px",
+    rotate: "45deg",
+    bottom: "240px",
+    left: "50px",
+  },
+  {
+    color: "yellow",
+    width: "140px",
+    bottom: "30%",
+    left: "35%",
+  },
+  {
+    color: "white",
+    width: "100px",
+    rotate: "50deg",
+    top: "40%",
+    left: "50%",
+  },
+  {
+    color: "yellow",
+    width: "50px",
+    rotate: "-20deg",
+    bottom: "120px",
+    left: "50%",
+  },
+  {
+    color: "yellow",
+    width: "120px",
+    rotate: "50deg",
+    bottom: "40px",
+    right: "60px",
+  },
+];
+
 const HomeBG: React.FC<Props> = (props: Props) => {
+  const size = useTypedSelector((state) => state.sizes);
+
+  const triangleProps: BGTriangleProp[] = sizeTypeJudge(size)(
+    smBGTriangleProps,
+    tabletBGTriangleProps,
+    pcBGTriangleProps
+  );
+
   return (
     <Wrapper>
       <BGWrapper>
-        <BGTriangle color="white" />
-        <BGTriangle color="yellow" top="300px" left="200px" rotate="180deg" />
-        <BGTriangle color="ange" top="300px" left="0px" rotate="180deg" />
+        {triangleProps.map((triangleProp) => {
+          return <BGTriangle {...triangleProp} />;
+        })}
       </BGWrapper>
       <ContentWrapper>{props.children}</ContentWrapper>
     </Wrapper>
