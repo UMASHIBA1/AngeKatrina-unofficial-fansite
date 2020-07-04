40ac402da6fa84bb9586ae0634e1f2bb202eda12
UMASHIBA <umashiba1@gmail.com>
2020-07-05 03:16:38 +0900

    [MOD]tablet用のHomeAnge作成

--- components/organisms/home/HomeAnge/HomeAnge.tsx
+++ components/organisms/home/HomeAnge/HomeAnge.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 import styled from "styled-components";
 import angeBasicImgPath from "../../../../public/imgs/ange-basic.png";
+import { sm_breakpoint } from "../../../../constants/breakpoints";
 
-const angeHeight = 500;
+const smAngeHeight = 500;
+const tabletAngeHeight = 800;
 
 const Img = styled.img.attrs({
   src: angeBasicImgPath,
   alt: "アンジュ画像",
 })`
   position: absolute;
-  bottom: -${angeHeight * (3 / 5)}px;
-  right: -60px;
+  bottom: -${smAngeHeight * (3 / 5)}px;
+  right: -${smAngeHeight * (1 / 8)}px;
   transform: rotate(-24deg);
-  height: ${angeHeight}px;
+  height: ${smAngeHeight}px;
   max-width: none;
+  @media (min-width: ${sm_breakpoint}px) {
+    bottom: -${tabletAngeHeight * (3 / 5)}px;
+    right: -${tabletAngeHeight * (1 / 8)}px;
+    height: ${tabletAngeHeight}px;
+  }
 `;
 
 const HomeAnge: React.FC = () => {
