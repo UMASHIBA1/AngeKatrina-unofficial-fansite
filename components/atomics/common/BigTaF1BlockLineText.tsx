3df58103ef7d6f04301566d60a16ded64aa604ca
UMASHIBA <umashiba1@gmail.com>
2020-07-07 22:09:08 +0900

    [ADD]BigTaF1BlockLineTextを作成

---
+++ components/atomics/common/BigTaF1BlockLineText.tsx
@@ -0,0 +1,32 @@
+import React, { ReactNode } from "react";
+import styled from "styled-components";
+import { TA_F1_BLOCK_LINE } from "../../../constants/cssProps";
+import {
+  ANGE_LIVE_BACK_COLOR,
+  ANGE_WHITE,
+  ANGE_RED,
+} from "../../../constants/colors";
+
+const TextMain = styled.div<{ color: Props["color"] }>`
+  ${TA_F1_BLOCK_LINE}
+  font-size: 36px;
+  color: ${({ color }) =>
+    color === "bgRed"
+      ? ANGE_LIVE_BACK_COLOR
+      : color === "angeRed"
+      ? ANGE_RED
+      : color === "white"
+      ? ANGE_WHITE
+      : null};
+`;
+
+interface Props {
+  color: "bgRed" | "angeRed" | "white";
+  children: ReactNode;
+}
+
+const BigTaF1BlockLineText: React.FC<Props> = (props: Props) => {
+  return <TextMain color={props.color}>{props.children}</TextMain>;
+};
+
+export default BigTaF1BlockLineText;
