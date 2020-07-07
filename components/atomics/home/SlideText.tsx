8b5af6f82cf8f04f2b5c422ff78aa781f11d642b
UMASHIBA <umashiba1@gmail.com>
2020-07-07 23:23:19 +0900

    [MOD]SlideTextにsm,tablet版を追加

--- components/atomics/home/SlideText.tsx
+++ components/atomics/home/SlideText.tsx
@@ -6,11 +6,23 @@ import {
   ANGE_WHITE,
   ANGE_RED,
 } from "../../../constants/colors";
+import {
+  sm_breakpoint,
+  tablet_breakpoint,
+} from "../../../constants/breakpoints";
 
 const TextMain = styled.div<{ color: Props["color"] }>`
   ${TA_F1_BLOCK_LINE}
-  font-size: 40px;
+  font-size: 1.2rem;
   line-height: 1.8;
+  @media (min-width: ${sm_breakpoint}px) {
+    font-size: 2.2rem;
+  }
+
+  @media (min-width: ${tablet_breakpoint}px) {
+    font-size: 2.5rem;
+  }
+
   color: ${({ color }) =>
     color === "bgRed"
       ? ANGE_LIVE_BACK_COLOR
