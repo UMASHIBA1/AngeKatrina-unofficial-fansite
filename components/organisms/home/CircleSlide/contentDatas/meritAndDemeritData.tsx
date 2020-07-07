48c667f8afa5b532a659a28a04cfd34699ce2a83
UMASHIBA <umashiba1@gmail.com>
2020-07-07 23:54:43 +0900

    [MOD]meritAndDemeritDataのメリットとデメリットを作成

--- components/organisms/home/CircleSlide/contentDatas/meritAndDemeritData.tsx
+++ components/organisms/home/CircleSlide/contentDatas/meritAndDemeritData.tsx
@@ -1,6 +1,7 @@
 import { Props as SlideContentProps } from "../SlideContent/SlideContent";
 import styled from "styled-components";
 import BigSlideText from "../../../../atomics/home/BigSlideText";
+import SlideText from "../../../../atomics/home/SlideText";
 
 const Wrapper = styled.div`
   width: 90%;
@@ -14,9 +15,30 @@ const FirstPage = () => (
   </Wrapper>
 );
 
+const SecondPage = () => (
+  <Wrapper>
+    <BigSlideText color="angeRed">メリット</BigSlideText>
+    <SlideText color="angeRed">とにかく可愛い</SlideText>
+    <SlideText color="angeRed">配信が楽しい</SlideText>
+    <SlideText color="angeRed">毎日の生きがいができる</SlideText>
+    <SlideText color="angeRed">つらい時思い出して元気になれる</SlideText>
+    <SlideText color="angeRed">とにかくかわいい</SlideText>
+  </Wrapper>
+);
+
+const ThirdPage = () => (
+  <Wrapper>
+    <BigSlideText color="angeRed">デメリット</BigSlideText>
+    <SlideText color="angeRed">骨抜きになってしまう</SlideText>
+    <SlideText color="angeRed">配信のない夜がとてもさみしい</SlideText>
+    <SlideText color="angeRed">コイビトがいる人はもめる原因に</SlideText>
+    <SlideText color="angeRed">とにかく可愛すぎる</SlideText>
+  </Wrapper>
+);
+
 const meritAndDemeritData: SlideContentProps = {
   animationType: "slide",
-  slidePages: [<FirstPage />],
+  slidePages: [<FirstPage />, <SecondPage />, <ThirdPage />],
 };
 
 export default meritAndDemeritData;
