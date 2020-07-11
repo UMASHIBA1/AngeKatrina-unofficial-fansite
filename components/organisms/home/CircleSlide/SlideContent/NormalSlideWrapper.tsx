648def67087e87e85aa1facfc7bab51d69f60797
UMASHIBA <umashiba1@gmail.com>
2020-07-11 15:01:21 +0900

    [ADD]slideAnimationを別ファイルとして分離

--- components/organisms/home/CircleSlide/SlideContent/NormalSlideWrapper.tsx
+++ components/organisms/home/CircleSlide/SlideContent/NormalSlideWrapper.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import { AnimationTimeProps } from "./SlideContent";
+import slideAnimation from "./slideAnimation";
 
 interface Props {
   slideNum: number;
@@ -8,39 +9,6 @@ interface Props {
   children: ReactNode;
   onSlideEndFC?: (event: React.AnimationEvent<HTMLDivElement>) => void;
 }
-
-const slideAnimation = (slideNum: number) => {
-  // NOTE スライドの枚数が変わってもうまくkeyframeの%を調整できるように下の処理で色々やってる
-  const props: { percentage: number; x: number }[] = [{ percentage: 0, x: 0 }];
-
-  const basePercentage = 100 / (slideNum * 3 - 1);
-  let previousNum = 0;
-  for (const num of [...Array(slideNum).keys()]) {
-    if (num !== slideNum - 1) {
-      const showSlidePercentage = previousNum + basePercentage * 2;
-      const x = num * -100;
-      props.push({ percentage: showSlidePercentage, x: x });
-      previousNum = showSlidePercentage;
-
-      const moveSlidePercentage = previousNum + basePercentage;
-      const next_x = (num + 1) * -100;
-      props.push({ percentage: moveSlidePercentage, x: next_x });
-      previousNum = moveSlidePercentage;
-    } else {
-      props.push({ percentage: 100, x: num * -100 });
-    }
-  }
-
-  return keyframes`
-	  ${props.map(({ percentage, x }) => {
-      return `
-		  ${percentage}% {
-		  transform: translate(${x}%, 0);
-		}
-		  `;
-    })}
-	`;
-};
 
 const SlideWrapper = styled.div<{
   slideNum: number;
