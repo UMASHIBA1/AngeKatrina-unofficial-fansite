d6580459157515a4bd9d85cfaad2f8e1922a3996
UMASHIBA <umashiba1@gmail.com>
2020-07-18 20:47:02 +0900

    [MOD]SideBarTItleをSideBarに追加

--- components/organisms/common/SideBar/SideBar.tsx
+++ components/organisms/common/SideBar/SideBar.tsx
@@ -3,6 +3,8 @@ import SideBarBG from "./SideBarBG";
 import useDidMount from "../../../../hooks/useDidMount";
 import CloseButton from "../../../atomics/common/CloseButton";
 import SideBarTopBar from "./SideBarTopBar/SideBarTopBar";
+import { ANGE_LIVE_BACK_COLOR } from "../../../../constants/colors";
+import SlideBarTitle from "./SideBarTopBar/SideBarTitle";
 
 const SideBar: React.FC = () => {
   const [isOpenSideBar, changeIsOpenSideBar] = useState(false);
@@ -16,6 +18,7 @@ const SideBar: React.FC = () => {
   return (
     <SideBarBG isOpenSideBar={isOpenSideBar}>
       <SideBarTopBar>
+        <SlideBarTitle text="どれにする？" color={ANGE_LIVE_BACK_COLOR} />
         <CloseButton
           onClickFC={() => {
             changeIsOpenSideBar(false);
