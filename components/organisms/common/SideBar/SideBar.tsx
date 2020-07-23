834d5a1cffe77e7d144a82170bab2da4f00faaae
UMASHIBA <umashiba1@gmail.com>
2020-07-23 23:43:09 +0900

    [MOD]sm用のサイドバーを作成

--- components/organisms/common/SideBar/SideBar.tsx
+++ components/organisms/common/SideBar/SideBar.tsx
@@ -3,9 +3,47 @@ import SideBarBG from "./SideBarBG";
 import useDidMount from "../../../../hooks/useDidMount";
 import CloseButton from "../../../atomics/common/CloseButton/CloseButton";
 import SideBarTopBar from "./SideBarTopBar/SideBarTopBar";
-import { ANGE_LIVE_BACK_COLOR } from "../../../../constants/colors";
+import { ANGE_LIVE_BACK_COLOR, ANGE_WHITE } from "../../../../constants/colors";
 import SlideBarTitle from "./SideBarTopBar/SideBarTitle";
 import SideBarMenu from "./SideBarMenu/SideBarMenu";
+import RedHomeIcon from "../../../../public/svgs/common/red-homeIcon.svg";
+import RedInfoIcon from "../../../../public/svgs/common/red-infoIcon.svg";
+import RedLicenseIcon from "../../../../public/svgs/common/red-licenseIcon.svg";
+import RedPresentIcon from "../../../../public/svgs/common/red-presentIcon.svg";
+import WhiteHomeIcon from "../../../../public/svgs/common/white-homeIcon.svg";
+import WhiteInfoIcon from "../../../../public/svgs/common/white-infoIcon.svg";
+import WhiteLicenseIcon from "../../../../public/svgs/common/white-licenseIcon.svg";
+import WhitePresentIcon from "../../../../public/svgs/common/white-presentIcon.svg";
+import ListRow from "../../../molecules/common/ListRow";
+
+interface contentDataType {
+  icon: React.StatelessComponent<React.SVGAttributes<SVGElement>>;
+  hoveredIcon: React.StatelessComponent<React.SVGAttributes<SVGElement>>;
+  mainText: string;
+}
+
+const contentData: contentDataType[] = [
+  {
+    icon: RedHomeIcon,
+    hoveredIcon: WhiteHomeIcon,
+    mainText: "ホーム",
+  },
+  {
+    icon: RedInfoIcon,
+    hoveredIcon: WhiteInfoIcon,
+    mainText: "アンジュ情報",
+  },
+  {
+    icon: RedLicenseIcon,
+    hoveredIcon: WhiteLicenseIcon,
+    mainText: "ライセンス",
+  },
+  {
+    icon: RedPresentIcon,
+    hoveredIcon: WhitePresentIcon,
+    mainText: "ガチャ",
+  },
+];
 
 const SideBar: React.FC = () => {
   const [isOpenSideBar, changeIsOpenSideBar] = useState(false);
@@ -27,7 +65,21 @@ const SideBar: React.FC = () => {
           right="10px"
         />
       </SideBarTopBar>
-      <SideBarMenu></SideBarMenu>
+      <SideBarMenu>
+        {contentData.map(({ icon, mainText, hoveredIcon }) => {
+          return (
+            <ListRow
+              key={mainText}
+              mainText={mainText}
+              IconSvg={icon}
+              HoveredSvg={hoveredIcon}
+              bgColor={ANGE_WHITE}
+              textColor={ANGE_LIVE_BACK_COLOR}
+              cursor={"pointer"}
+            />
+          );
+        })}
+      </SideBarMenu>
     </SideBarBG>
   );
 };
