dfd7b9b72a662bd38791eb9a5035d1a2a8efeb2e
UMASHIBA <umashiba1@gmail.com>
2021-01-01 16:19:51 +0900

    mod: HomeAnge変更

--- components/organisms/home/HomeAnge/HomeAnge.tsx
+++ components/organisms/home/HomeAnge/HomeAnge.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+import styled, { keyframes } from "styled-components";
 import angeBasicImgPath from "../../../../public/imgs/ange-basic.png";
 import {
   sm_breakpoint,
@@ -8,6 +8,23 @@ import {
 import SpeechBubble, { Props as SpeechBubbleProp } from "./SpeechBubble";
 import { useTypedSelector } from "../../../../redux/store";
 import sizeTypeJudge from "../../../../systems/sizeTypeJudge";
+
+const angeTopDownDuration_ms = 200;
+
+const angeTopDownAnimation = keyframes`
+  60% {
+    margin-bottom: 20px;
+  }
+
+  70% {
+    margin-bottom: 20px;
+  }
+
+  100% {
+    margin-bottom: 0;
+  }
+
+`;
 
 // アンジュ画像の大体の縦横比
 const angeImgAspectRatio = 0.356;
@@ -19,13 +36,18 @@ const pcAngeHeight = "150vh";
 const Img = styled.img.attrs({
   src: angeBasicImgPath,
   alt: "アンジュ画像",
-})`
+})<{ isStartAnimation: boolean }>`
   position: absolute;
   bottom: -${smAngeHeight * (3 / 5)}px;
   right: -${smAngeHeight * (1 / 8)}px;
   transform: rotate(-24deg);
   height: ${smAngeHeight}px;
   max-width: none;
+  cursor: pointer;
+  margin-bottom: 0;
+  animation: ${({ isStartAnimation }) =>
+      isStartAnimation ? angeTopDownAnimation : "none"}
+    ${angeTopDownDuration_ms}ms ease-in forwards;
   @media (min-width: ${sm_breakpoint}px) {
     bottom: -${tabletAngeHeight * (3 / 5)}px;
     right: -${tabletAngeHeight * (1 / 8)}px;
@@ -64,8 +86,38 @@ const pcSpeechBubbleProp: Omit<SpeechBubbleProp, "text"> = {
   bottom: `calc(${pcAngeHeight} * 0.4)`,
 };
 
+const useAngeComment = () => {
+  const angeCommentList = [
+    "ほにゅ？",
+    "わっ！",
+    "死ぬんだぁ",
+    "もろて",
+    "それはそう",
+    "ぺったんこ！",
+  ];
+  // NOTE サーバー側とクライアントの表示コンテンツを合わせる為最初はindex0
+  const randomIndex = 0;
+  const [angeCommentNowIndex, _changeAngeComment] = useState(randomIndex);
+
+  const changeAngeComment = () => {
+    let randomIndex = Math.floor(Math.random() * angeCommentList.length);
+    while (randomIndex == angeCommentNowIndex) {
+      randomIndex = Math.floor(Math.random() * angeCommentList.length);
+    }
+    _changeAngeComment(randomIndex);
+  };
+  return [angeCommentList[angeCommentNowIndex], changeAngeComment] as [
+    string,
+    () => void
+  ];
+};
+
 const HomeAnge: React.FC = () => {
   const size = useTypedSelector((state) => state.sizes);
+  const [angeComment, changeAngeComment] = useAngeComment();
+  const [isStartAngeTopDownAnimation, changeTopDownAnimationStatus] = useState(
+    false
+  );
 
   const bubbleProp = sizeTypeJudge(size)(
     smSpeechBubbleProp,
@@ -74,8 +126,15 @@ const HomeAnge: React.FC = () => {
   );
   return (
     <React.Fragment>
-      <Img />
-      <SpeechBubble text="ほにゅ" {...bubbleProp} />
+      <Img
+        isStartAnimation={isStartAngeTopDownAnimation}
+        onClick={() => {
+          changeAngeComment();
+          changeTopDownAnimationStatus(true);
+        }}
+        onAnimationEnd={() => changeTopDownAnimationStatus(false)}
+      />
+      <SpeechBubble text={angeComment} {...bubbleProp} />
     </React.Fragment>
   );
 };
