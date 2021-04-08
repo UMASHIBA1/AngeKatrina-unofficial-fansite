92508d220c9c4ac54ccb652d2c8c04269d69ccb8
UMASHIBA <umashiba1@gmail.com>
2021-04-08 14:46:09 +0900

    mod: change storybook setting for loading svg through svgr

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,4 +5,16 @@ module.exports = {
     "../components/**/*.stories.tsx",
   ],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  webpackFinal: async (config) => {
+    const rules = config.module.rules;
+    const fileLoaderRule = rules.find((rule) => rule.test.test(".svg"));
+    fileLoaderRule.exclude = /\.svg$/;
+
+    rules.push({
+      test: /\.svg$/,
+      enforce: "pre",
+      loader: require.resolve("@svgr/webpack"),
+    });
+    return config;
+  },
 };
