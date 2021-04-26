8c00e2d264b8d3d7e21060d42f1a996965ffb503
UMASHIBA <umashiba1@gmail.com>
2021-04-26 16:19:12 +0900

    mod: create animation for SVGCircle

--- components/organisms/common/NavBar/NavBarLeft/CenterCircle/CenterCircle.tsx
+++ components/organisms/common/NavBar/NavBarLeft/CenterCircle/CenterCircle.tsx
@@ -1,18 +1,25 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import FirstInsideCircle from "../../../../../../public/svgs/common/one_yellow_circle.svg";
 import SecondInsideCircle from "../../../../../../public/svgs/common/one_black_circle.svg";
 import ThirdInsideCircle from "../../../../../../public/svgs/common/two_yellow_two_red_circle.svg";
 import { TA_F1_BLOCK_LINE } from "../../../../../../constants/cssProps";
 import { ANGE_RED } from "../../../../../../constants/colors";
 import OutsideRotateLine from "./OutsideRotateLine";
+import { scale } from "../../../../../../styles/commonAnimation";
 
-const SVGWrapper = styled.div<{ width: number }>`
+const SVGWrapper = styled.div<{ width: number; animationDelay: number }>`
   position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  ${({ width }) =>
+    width &&
+    css`
+      top: calc(50% - width / 2);
+      left: calc(50% - width / 2);
+    `}
   width: ${({ width }) => width}px;
+  transform: scale(0);
+  animation: ${scale(1)} 800ms ease-out forwards
+    ${({ animationDelay }) => animationDelay}ms;
 `;
 
 const Wrapper = styled.div`
@@ -63,13 +70,13 @@ const CenterCircle: React.FC = () => {
         startRotateDeg={240}
         animationDelayMs={200}
       />
-      <SVGWrapper width={450}>
+      <SVGWrapper width={450} animationDelay={200}>
         <ThirdInsideCircle />
       </SVGWrapper>
-      <SVGWrapper width={415}>
+      <SVGWrapper width={415} animationDelay={350}>
         <SecondInsideCircle />
       </SVGWrapper>
-      <SVGWrapper width={380}>
+      <SVGWrapper width={380} animationDelay={500}>
         <FirstInsideCircle />
       </SVGWrapper>
       <CenterText>どれにする？</CenterText>
