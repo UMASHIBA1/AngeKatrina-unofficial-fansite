be2ac35c68d9125522d5a912cc0725697ca1ae3b
UMASHIBA <umashiba1@gmail.com>
2021-11-27 02:12:59 +0900

    add: setting static ogp

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import ogpImg from "../public/imgs/ogp.png";
 
+const hostUrl = "https://ange-kawaina.vercel.app/";
+const ogpAbsolutePath = hostUrl + ogpImg;
 class MyDocument extends Document {
   render() {
     return (
@@ -9,6 +12,18 @@ class MyDocument extends Document {
           <title>非公式ファンサイト アンゲ・カワイーナ</title>
           <meta
             name="description"
+            content="にじさんじライバー アンジュカトリーナさんの非公式ファンサイトです。楽しんでいってね！"
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            property="og:title"
+            content="非公式ファンサイト アンゲ・カワイーナ"
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content={hostUrl} />
+          <meta property="og:image" content={ogpAbsolutePath} />
+          <meta
+            property="og:description"
             content="にじさんじライバー アンジュカトリーナさんの非公式ファンサイトです。楽しんでいってね！"
           />
           <script src="/load-adobe-fonts.js" />
