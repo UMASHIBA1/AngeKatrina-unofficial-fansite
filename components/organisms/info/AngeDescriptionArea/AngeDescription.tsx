35d373f207b1a91cbb79f9861cbb23fc197d81ff
UMASHIBA <umashiba1@gmail.com>
2021-12-23 18:06:13 +0900

    mod: change Background for smp

--- components/organisms/info/AngeDescriptionArea/AngeDescription.tsx
+++ components/organisms/info/AngeDescriptionArea/AngeDescription.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import { sm_breakpoint } from "../../../../constants/breakpoints";
-import { ANGE_LIVE_BACK_COLOR } from "../../../../constants/colors";
+import {
+  ANGE_LIVE_BACK_COLOR,
+  ANGE_WHITE_TRANSLUCENT,
+} from "../../../../constants/colors";
 import { BUNKYU_MIDASHI_GO_STD } from "../../../../constants/cssProps";
 
 const Wrapper = styled.div`
@@ -13,6 +16,7 @@ const Wrapper = styled.div`
   color: ${ANGE_LIVE_BACK_COLOR};
   border: 4px solid ${ANGE_LIVE_BACK_COLOR};
   border-radius: 16px;
+  background-color: ${ANGE_WHITE_TRANSLUCENT};
 `;
 
 const Title = styled.h1`
