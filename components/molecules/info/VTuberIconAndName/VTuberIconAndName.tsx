f7111d118c5fb38a8071ae1d69d3969c0829d801
UMASHIBA <umashiba1@gmail.com>
2021-12-24 11:41:03 +0900

    mod: change VTuberIconAndName

--- components/molecules/info/VTuberIconAndName/VTuberIconAndName.tsx
+++ components/molecules/info/VTuberIconAndName/VTuberIconAndName.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { sm_breakpoint } from "../../../../constants/breakpoints";
 import { ANGE_LIVE_BACK_COLOR } from "../../../../constants/colors";
 import { BUNKYU_MIDASHI_GO_STD } from "../../../../constants/cssProps";
 import VTuberIcon from "../../../atomics/info/VTuberIcon/VTuberIcon";
@@ -17,10 +18,15 @@ const Wrappper = styled.div`
 `;
 
 const VTuberName = styled.p`
-  font-size: 1.2rem;
+  font-size: 1rem;
   font-weight: bold;
   ${BUNKYU_MIDASHI_GO_STD}
   color: ${ANGE_LIVE_BACK_COLOR};
+  margin-top: 28px;
+
+  @media (min-width: ${sm_breakpoint}px) {
+    font-size: 1.2rem;
+  }
 `;
 
 const VTuberIconAndName: React.VFC<VTuberIconAndNameProps> = ({
