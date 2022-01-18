b69a75d2b886db4e8df99e7eeecc84d7d5a3d69b
UMASHIBA <umashiba1@gmail.com>
2022-01-19 01:58:19 +0900

    mod: change EmbedBox padding

--- components/atomics/info/EmbedBox/EmbedBox.tsx
+++ components/atomics/info/EmbedBox/EmbedBox.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from "react";
 import styled from "styled-components";
+import { sm_breakpoint } from "../../../../constants/breakpoints";
 import { ANGE_LIVE_BACK_COLOR } from "../../../../constants/colors";
 import { BUNKYU_MIDASHI_GO_STD } from "../../../../constants/cssProps";
 
@@ -18,7 +19,11 @@ const Wrapper = styled.section`
   border: 4px solid ${ANGE_LIVE_BACK_COLOR};
   border-radius: 24px;
   overflow-x: hidden;
-  padding: 16px;
+  padding: 16px 16px 32px 16px;
+
+  @media (min-width: ${sm_breakpoint}px) {
+    padding: 16px 16px 64px 16px;
+  }
 `;
 
 const Title = styled.h1`
