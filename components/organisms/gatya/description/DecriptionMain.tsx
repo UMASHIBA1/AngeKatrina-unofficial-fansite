d014bde574a34b2d7a4963233380e6f1e61d5133
UMASHIBA <umashiba1@gmail.com>
2022-04-18 00:17:31 +0900

    mod: create animation for gatya description page

--- components/organisms/gatya/description/DecriptionMain.tsx
+++ components/organisms/gatya/description/DecriptionMain.tsx
@@ -7,6 +7,40 @@ import AngeHeyImg from "../../../../public/imgs/gatya/ange-hey.png";
 import { BUNKYU_MIDASHI_GO_STD } from "../../../../constants/cssProps";
 import { sm_breakpoint } from "../../../../constants/breakpoints";
 import Link from "next/link";
+import {
+  boundExpand,
+  colorChange,
+  fadein,
+  multiBoundExpand,
+  scale,
+  translate,
+} from "../../../../styles/commonAnimation";
+import AnimationProps from "../../../../typing/AnimationProps";
+
+const backgroundAnimation: AnimationProps = {
+  delay_ms: 300,
+  duration_ms: 500,
+};
+
+const titleAnimation: AnimationProps = {
+  delay_ms: backgroundAnimation.delay_ms,
+  duration_ms: 300,
+};
+
+const descriptionTextAnimation: AnimationProps = {
+  delay_ms: titleAnimation.delay_ms + 50,
+  duration_ms: 600,
+};
+
+const startButtonAnimation: AnimationProps = {
+  delay_ms: descriptionTextAnimation.delay_ms + 50,
+  duration_ms: 600,
+};
+
+const warningTextAnimation: AnimationProps = {
+  delay_ms: startButtonAnimation.delay_ms + 350,
+  duration_ms: 500,
+};
 
 const Wrapper = styled.div`
   position: relative;
@@ -19,14 +53,23 @@ const Wrapper = styled.div`
   max-height: 600px;
   border: 4px solid ${ANGE_LIVE_BACK_COLOR};
   border-radius: 24px;
+  animation: ${fadein(1)} ${backgroundAnimation.duration_ms - 100}ms both
+    ${backgroundAnimation.delay_ms - 100}ms linear;
 `;
 
-const BackgroundImg = styled.img<{ top: string; left: string; rotate: string }>`
+const BackgroundImg = styled.img<{
+  top: string;
+  left: string;
+  rotate: string;
+  delayMs: number;
+}>`
   position: absolute;
   top: ${({ top }) => top};
   left: ${({ left }) => left};
   transform: rotate(${({ rotate }) => rotate});
   object-fit: "contain";
+  animation: ${fadein(1)} ${backgroundAnimation.duration_ms}ms both
+    ${({ delayMs }) => backgroundAnimation.delay_ms + delayMs}ms linear;
 `;
 
 const DescriptionContent = styled.main`
@@ -48,19 +91,62 @@ const TitleArea = styled.div`
 `;
 
 const Title = styled.h1`
+  position: relative;
+  top: 0;
+  left: 0;
   width: 240px;
   font-size: 1.75rem;
-  border: solid ${ANGE_LIVE_BACK_COLOR};
-  border-width: 0 0 2px 0;
   text-align: center;
   padding: 4px 0;
+  overflow: hidden;
+  animation: ${colorChange(ANGE_WHITE, ANGE_LIVE_BACK_COLOR)} 0ms ease-in-out
+    ${titleAnimation.delay_ms + titleAnimation.duration_ms / 2}ms both;
+
+  :after {
+    position: absolute;
+    top: 0;
+    left: 0;
+    content: "";
+    width: 100%;
+    height: 100%;
+    background-color: ${ANGE_LIVE_BACK_COLOR};
+    animation: ${translate({ x: "-100%", y: 0 }, { x: "100%", y: 0 })}
+      ${titleAnimation.duration_ms}ms ease-in-out ${titleAnimation.delay_ms}ms
+      both;
+  }
 `;
 
-const DescriptionText = styled.p`
+const DescriptionTextWrapper = styled.div`
   width: 94%;
   text-align: center;
   font-size: 1.25rem;
-  line-height: 2.5rem;
+`;
+
+const AnimateDescriptionText = styled.div<{ delayMs: number }>`
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 100%;
+  overflow: hidden;
+  margin: 4px 0;
+  animation: ${colorChange(ANGE_WHITE, ANGE_LIVE_BACK_COLOR)} 0ms ease-in-out
+    ${({ delayMs }) =>
+      descriptionTextAnimation.delay_ms +
+      descriptionTextAnimation.duration_ms / 2 +
+      delayMs}ms
+    both;
+  :after {
+    position: absolute;
+    top: 0;
+    left: 0;
+    content: "";
+    width: 100%;
+    height: 100%;
+    background-color: ${ANGE_LIVE_BACK_COLOR};
+    animation: ${translate({ x: "-100%", y: 0 }, { x: "100%", y: 0 })}
+      ${descriptionTextAnimation.duration_ms}ms ease-in-out
+      ${({ delayMs }) => descriptionTextAnimation.delay_ms + delayMs}ms both;
+  }
 `;
 
 const StartButton = styled.button`
@@ -80,6 +166,13 @@ const StartButton = styled.button`
     color: ${ANGE_WHITE};
   }
 
+  transform: scale(0);
+
+  animation: ${scale(1)} ease-out ${startButtonAnimation.duration_ms - 200}ms
+      both ${startButtonAnimation.delay_ms}ms,
+    ${fadein()} ${startButtonAnimation.duration_ms}ms ease-out
+      ${startButtonAnimation.delay_ms + 100}ms both;
+
   @media (min-width: ${sm_breakpoint}px) {
     width: 360px;
   }
@@ -93,12 +186,15 @@ const WarningText = styled.p`
   background-color: ${ANGE_WHITE};
   border-radius: 8px;
   padding: 4px;
+  animation: ${fadein()} ease-out ${warningTextAnimation.duration_ms}ms both
+    ${warningTextAnimation.delay_ms}ms;
 `;
 
 const DescriptionMain: React.VFC = () => {
   return (
     <Wrapper>
       <BackgroundImg
+        delayMs={0}
         height="240px"
         top="-20%"
         left="70%"
@@ -107,6 +203,7 @@ const DescriptionMain: React.VFC = () => {
         src={AngeLightDressImg}
       />
       <BackgroundImg
+        delayMs={200}
         height="400px"
         top="70%"
         left="-10%"
@@ -115,6 +212,7 @@ const DescriptionMain: React.VFC = () => {
         src={AngeBasicImg}
       />
       <BackgroundImg
+        delayMs={400}
         height="500px"
         top="46%"
         left="84%"
@@ -126,13 +224,17 @@ const DescriptionMain: React.VFC = () => {
         <TitleArea>
           <Title>アンジュガチャ！</Title>
         </TitleArea>
-        <DescriptionText>
-          お楽しみ要素としてアンジュが出てくるガチャガチャを作ったよ！
-          <br />
-          出てくるアンジュの衣装は3種類！
-          <br />
-          ぜひ遊んでみてね！
-        </DescriptionText>
+        <DescriptionTextWrapper>
+          <AnimateDescriptionText delayMs={0}>
+            お楽しみ要素としてアンジュが出てくるガチャガチャを作ったよ！
+          </AnimateDescriptionText>
+          <AnimateDescriptionText delayMs={100}>
+            出てくるアンジュの衣装は3種類！
+          </AnimateDescriptionText>
+          <AnimateDescriptionText delayMs={200}>
+            ぜひ遊んでみてね！
+          </AnimateDescriptionText>
+        </DescriptionTextWrapper>
         <Link href="/gatya">
           <StartButton>ガチャスタート！</StartButton>
         </Link>
