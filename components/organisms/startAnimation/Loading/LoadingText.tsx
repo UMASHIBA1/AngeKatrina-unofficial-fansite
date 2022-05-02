5a361c06ca79ed8b4ef67caac88582ad5df4df3a
UMASHIBA <umashiba1@gmail.com>
2022-05-02 22:48:47 +0900

    mod: create loadingText disappear animation

--- components/organisms/startAnimation/Loading/LoadingText.tsx
+++ components/organisms/startAnimation/Loading/LoadingText.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import styled, { keyframes } from "styled-components";
+import React, { useState } from "react";
+import styled, { css, keyframes } from "styled-components";
 import { ANGE_BLACK } from "../../../../constants/colors";
 import { HWT_MARDELL_FONT_PROP } from "../../../../constants/cssProps";
 import useAnimationRestarter from "../../../../hooks/useAnimationRestarter";
@@ -14,32 +14,88 @@ const textAnimation = keyframes`
     }
 `;
 
+const disappearAnimation = keyframes`
+  0% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+
+
+  100% {
+    transform: translateY(16px);
+    opacity: 0;
+  }
+`;
+
 const Wrapper = styled.div`
   font-size: 1rem;
   ${HWT_MARDELL_FONT_PROP}
   color: ${ANGE_BLACK};
 `;
 
-const Text = styled.p<{ delayMs: number }>`
+type animationKind = "loading" | "disappear";
+
+const Text = styled.p<{ animationKind: animationKind; delayMs: number }>`
   display: inline-block;
-  animation: ${textAnimation} 600ms ease-out ${({ delayMs }) => delayMs}ms both;
+  ${({ animationKind, delayMs }) =>
+    animationKind === "loading" &&
+    css`
+      animation: ${textAnimation} 600ms ease-out ${delayMs}ms both;
+    `}
+  ${({ animationKind, delayMs }) =>
+    animationKind === "disappear" &&
+    css`
+      animation: ${disappearAnimation} 600ms ease-out ${delayMs}ms both;
+    `}
 `;
 
 const loadingText = "Loading";
 
-const delayInterval = 100;
+const loadingDelayInterval = 100;
+const disappearDelayInterval = 50;
+
+const useAnimation = () => {
+  const [animationKind, changeAnimationKind] = useState<animationKind>(
+    "loading"
+  );
+
+  const toNextAnimation = () => {
+    changeAnimationKind("disappear");
+  };
+
+  return [animationKind, toNextAnimation] as [
+    animationKind,
+    typeof toNextAnimation
+  ];
+};
 
 const LoadingText: React.VFC = () => {
-  const [key, restartAnimation] = useAnimationRestarter();
+  const [animationKind, toNextAnimation] = useAnimation();
 
   return (
-    <Wrapper key={key}>
+    <Wrapper>
       {loadingText.split("").map((char, i) => {
-        return <Text delayMs={delayInterval * i + 300}>{char}</Text>;
+        return (
+          <Text
+            animationKind={animationKind}
+            delayMs={
+              animationKind === "loading"
+                ? loadingDelayInterval * i + 300
+                : disappearDelayInterval * i
+            }
+          >
+            {char}
+          </Text>
+        );
       })}
       <Text
-        delayMs={delayInterval * loadingText.length + 300}
-        onAnimationEnd={restartAnimation}
+        animationKind={animationKind}
+        delayMs={
+          animationKind === "loading"
+            ? loadingDelayInterval * loadingText.length + 300
+            : disappearDelayInterval * loadingText.length
+        }
+        onAnimationEnd={toNextAnimation}
       >
         ...
       </Text>
