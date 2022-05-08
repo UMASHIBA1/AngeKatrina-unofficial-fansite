51ed86da9a5584bc8e75aa999c8fd5e3e8e396f7
UMASHIBA <umashiba1@gmail.com>
2022-05-08 22:16:12 +0900

    mod: create three squares collect animation ver1

--- components/organisms/startAnimation/Loading/ThreeSquares.tsx
+++ components/organisms/startAnimation/Loading/ThreeSquares.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styled, { css, keyframes } from "styled-components";
 import { ANGE_BLACK, ANGE_YELLOW } from "../../../../constants/colors";
-import useAnimationRestarter from "../../../../hooks/useAnimationRestarter";
+
+type animationKind = "loading" | "disappear";
 
 const rotateSquare = keyframes`
     0% {
@@ -31,15 +32,42 @@ const squareColorChange = keyframes`
     }
 `;
 
-const Square = styled.div<{ delayMs: number }>`
+const toCenter = (toX: string) => keyframes`
+  0% {
+    transform: translateX(0) rotate(0);
+  }
+
+  100% {
+    transform: translateX(${toX}) rotate(2turn);
+  }
+`;
+
+const Square = styled.div<{ animationKind: animationKind; delayMs: number }>`
   width: 32px;
   height: 32px;
   border-radius: 8px;
   background-color: ${ANGE_YELLOW};
-  ${({ delayMs }) =>
+  ${({ animationKind, delayMs }) =>
+    animationKind === "loading" &&
     css`
       animation: ${rotateSquare} 600ms ease-in-out both ${delayMs}ms,
         ${squareColorChange} 600ms forwards ${delayMs}ms;
+    `}
+  ${({ animationKind, delayMs }) =>
+    animationKind === "disappear" &&
+    css`
+      animation: ${rotateSquare} 600ms ease-in-out both ${delayMs}ms,
+        ${squareColorChange} 600ms forwards ${delayMs}ms;
+
+      :nth-child(1) {
+        transform-origin: 52px 0;
+        animation: ${toCenter("52px")} 400ms ease-out 1200ms both;
+      }
+
+      :nth-child(3) {
+        transform-origin: -52px 0;
+        animation: ${toCenter("-52px")} 400ms ease-out 1200ms both;
+      }
     `}
 `;
 
@@ -50,14 +78,33 @@ const Wrapper = styled.div`
   gap: 20px;
 `;
 
+const useAnimation = () => {
+  const [animationKind, changeAnimationKind] = useState<animationKind>(
+    "loading"
+  );
+
+  const toNextAnimation = () => {
+    changeAnimationKind("disappear");
+  };
+
+  return [animationKind, toNextAnimation] as [
+    animationKind,
+    typeof toNextAnimation
+  ];
+};
+
 const ThreeSquares: React.VFC = () => {
-  const [key, restartAnimation] = useAnimationRestarter();
+  const [animationKind, toNextAnimation] = useAnimation();
 
   return (
-    <Wrapper key={key}>
-      <Square delayMs={200} />
-      <Square delayMs={600} />
-      <Square delayMs={1000} onAnimationEnd={restartAnimation} />
+    <Wrapper>
+      <Square animationKind={animationKind} delayMs={200} />
+      <Square animationKind={animationKind} delayMs={600} />
+      <Square
+        animationKind={animationKind}
+        delayMs={1000}
+        onAnimationEnd={toNextAnimation}
+      />
     </Wrapper>
   );
 };
