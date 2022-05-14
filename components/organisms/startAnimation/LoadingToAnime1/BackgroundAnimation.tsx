a69381814d6e29b92aee9d27eedaee11ee61fcb9
UMASHIBA <umashiba1@gmail.com>
2022-05-14 19:03:57 +0900

    mod: create background animation ver1

--- components/organisms/startAnimation/LoadingToAnime1/BackgroundAnimation.tsx
+++ components/organisms/startAnimation/LoadingToAnime1/BackgroundAnimation.tsx
@@ -8,17 +8,21 @@ interface Props {
   isStartAnimation: boolean;
 }
 
+const animateBackgroundWidth = 4500;
+
+const wrapperWidth = `calc(${animateBackgroundWidth}vh + 100vw)`;
+
 const Wrapper = styled.div<{ isStartAnimation: boolean }>`
-  position: relative;
+  position: absolute;
   top: 0;
-  left: -100vw;
-  width: 200vw;
+  right: 0;
+  width: ${wrapperWidth};
   height: 100%;
-  overflow: hidden;
+  overflow: visible;
   ${({ isStartAnimation }) =>
     isStartAnimation &&
     css`
-      animation: ${translate({ x: 0, y: 0 }, { x: "100vw", y: 0 })}
+      animation: ${translate({ x: 0, y: 0 }, { x: wrapperWidth, y: 0 })}
         ${backgroundAnimation.duration_ms}ms linear
         ${backgroundAnimation.delay_ms}ms both;
     `}
@@ -27,28 +31,28 @@ const Wrapper = styled.div<{ isStartAnimation: boolean }>`
 const BackGround = styled.div`
   position: absolute;
   top: 0;
-  left: 50%;
-  width: 50%;
+  right: 0;
+  width: 100vw;
   height: 100%;
   background-color: ${ANGE_LIVE_BACK_COLOR};
 `;
 
 const AnimateHalfBackground = styled.div<{ position: "up" | "down" }>`
   position: absolute;
-  left: 0;
+  right: 100vw;
   ${({ position }) =>
     position === "up" &&
     css`
       bottom: 50vh;
-      border-bottom-left-radius: 150vw 50vh;
+      border-bottom-left-radius: ${animateBackgroundWidth}vh 150vh;
     `}
   ${({ position }) =>
     position === "down" &&
     css`
       top: 50vh;
-      border-top-left-radius: 150vw 50vh;
+      border-top-left-radius: ${animateBackgroundWidth}vh 150vh;
     `}
-  width: 100vw;
+  width: max(${animateBackgroundWidth / 3}vh, 100vw);
   height: 50vh;
   background-color: ${ANGE_LIVE_BACK_COLOR};
 `;
