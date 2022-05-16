8bcc428c995b615f00fe3963f35343dee33b5e86
UMASHIBA <umashiba1@gmail.com>
2022-05-16 23:21:05 +0900

    mod: refactor AnimateSquare

--- components/organisms/startAnimation/LoadingToAnime1/AnimateSquare.tsx
+++ components/organisms/startAnimation/LoadingToAnime1/AnimateSquare.tsx
@@ -1,9 +1,5 @@
 import React, { useEffect, useState } from "react";
 import styled, { css, keyframes } from "styled-components";
-import {
-  sm_breakpoint,
-  tablet_breakpoint,
-} from "../../../../constants/breakpoints";
 import { ANGE_YELLOW } from "../../../../constants/colors";
 import {
   squareMoveToLeft,
@@ -27,35 +23,25 @@ const moveToRight = keyframes`
   }
 `;
 
-const moveToLeft = (positionX: string, scale: number) => keyframes`
+const moveToLeft = () => keyframes`
     0% {
         transform: translateX(64px) scale(0.7);
     }
 
     100% {
-        transform: translateX(${positionX}) scale(${scale}) rotate(-45deg);
+        transform: translateX(calc(-50vw + 42px)) scale(2) rotate(-45deg);
     }
 `;
 
-const smpMoveToLeft = moveToLeft("calc(-50vw + 42px)", 2);
-
-const tabletMoveToLeft = moveToLeft("calc(-50vw + 42px)", 2);
-
-const pcMoveToLeft = moveToLeft("calc(-50vw + 58px)", 2);
-
-const openAnime1Move = (startPositionX: string, scale: number) => keyframes`
+const openAnime1Move = keyframes`
   0% {
-    transform: translateX(${startPositionX}) scale(${scale}) rotate(-45deg);
+    transform: translateX(calc(-50vw + 42px)) scale(2) rotate(-45deg);
   }
 
   100% {
     transform: translateX(calc(50vw + 58px)) scale(0.6) rotate(180deg);
   }
 `;
-
-const smpOpenAnime1 = openAnime1Move("calc(-50vw + 42px)", 2);
-const tabletOpenAnime1 = openAnime1Move("calc(-50vw + 42px)", 2);
-const pcOpenAnime1 = openAnime1Move("calc(-50vw + 58px)", 2);
 
 const Square = styled.div<{ animationPhase: animationPhase }>`
   width: 32px;
@@ -71,31 +57,15 @@ const Square = styled.div<{ animationPhase: animationPhase }>`
   ${({ animationPhase }) =>
     animationPhase === "moveToLeft" &&
     css`
-      animation: ${smpMoveToLeft} ${squareMoveToLeft.duration_ms}ms
+      animation: ${moveToLeft} ${squareMoveToLeft.duration_ms}ms
         cubic-bezier(0.36, 0.64, 0.46, 1) ${squareMoveToLeft.delay_ms}ms both;
-      @media (min-width: ${sm_breakpoint}px) {
-        animation: ${tabletMoveToLeft} ${squareMoveToLeft.duration_ms}ms
-          cubic-bezier(0.36, 0.64, 0.46, 1) ${squareMoveToLeft.delay_ms}ms both;
-      }
-      @media (min-width: ${tablet_breakpoint}px) {
-        animation: ${pcMoveToLeft} ${squareMoveToLeft.duration_ms}ms
-          cubic-bezier(0.36, 0.64, 0.46, 1) ${squareMoveToLeft.delay_ms}ms both;
-      }
     `}
 
     ${({ animationPhase }) =>
     animationPhase === "openAnime1" &&
     css`
-      animation: ${smpOpenAnime1} ${squareOpenAnime1.duration_ms}ms linear
+      animation: ${openAnime1Move} ${squareOpenAnime1.duration_ms}ms linear
         ${squareOpenAnime1.delay_ms}ms both;
-      @media (min-width: ${sm_breakpoint}px) {
-        animation: ${tabletOpenAnime1} ${squareOpenAnime1.duration_ms}ms linear
-          ${squareOpenAnime1.delay_ms}ms both;
-      }
-      @media (min-width: ${tablet_breakpoint}px) {
-        animation: ${pcOpenAnime1} ${squareOpenAnime1.duration_ms}ms linear
-          ${squareOpenAnime1.delay_ms}ms both;
-      }
     `}
 `;
 
