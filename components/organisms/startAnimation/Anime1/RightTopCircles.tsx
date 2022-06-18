4d29b7eed6918e8bf623e69f77f219cf4c8fb579
UMASHIBA <umashiba1@gmail.com>
2022-06-18 19:02:26 +0900

    mod: create RightTopCircles animation ver2

--- components/organisms/startAnimation/Anime1/RightTopCircles.tsx
+++ components/organisms/startAnimation/Anime1/RightTopCircles.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+import styled, { css } from "styled-components";
 import {
   ANGE_BLACK,
   ANGE_LIVE_BACK_COLOR,
@@ -37,7 +37,7 @@ const Wrapper = styled.div`
   height: 30vh;
 `;
 
-const YellowCircle = styled.div`
+const YellowCircle = styled.div<{ isStartDisappear: boolean }>`
   position: absolute;
   top: 16px;
   left: 0;
@@ -45,16 +45,28 @@ const YellowCircle = styled.div`
   height: 48px;
   background-color: ${ANGE_YELLOW};
   border-radius: 50%;
-  transform: scale(0);
-  /* 0.225 = 45vw / 200vw */
-  animation: ${scale(1)} ${yellowLineAnimationProps.duration_ms / 2}ms
-    ease-in-out
-    ${yellowLineAnimationProps.delay_ms +
-    yellowLineAnimationProps.duration_ms * 0.225}ms
-    forwards 2 alternate;
+  ${({ isStartDisappear }) =>
+    !isStartDisappear
+      ? css`
+          transform: scale(0);
+          /* 0.225 = 45vw / 200vw */
+          animation: ${scale(1)} ${yellowLineAnimationProps.duration_ms / 2}ms
+            ease-in-out
+            ${yellowLineAnimationProps.delay_ms +
+            yellowLineAnimationProps.duration_ms * 0.225}ms
+            forwards;
+        `
+      : css`
+          transform: scale(1);
+          animation: ${scale(0)} ${yellowLineAnimationProps.duration_ms / 2}ms
+            ease-in-out
+            ${yellowLineAnimationProps.delay_ms +
+            yellowLineAnimationProps.duration_ms * 0.225}ms
+            forwards;
+        `}
 `;
 
-const RedCircle = styled.div`
+const RedCircle = styled.div<{ isStartDisappear: boolean }>`
   position: absolute;
   top: 25vh;
   left: 14%;
@@ -62,15 +74,28 @@ const RedCircle = styled.div`
   height: 102px;
   background-color: ${ANGE_LIVE_BACK_COLOR};
   border-radius: 50%;
-  transform: scale(0);
-  /* 0.375 = 75vh / 200vh */
-  animation: ${scale(1)} ${redLineAnimationProps.duration_ms / 2}ms ease-in-out
-    ${redLineAnimationProps.delay_ms +
-    redLineAnimationProps.duration_ms * 0.375}ms
-    forwards 2 alternate;
+  ${({ isStartDisappear }) =>
+    !isStartDisappear
+      ? css`
+          transform: scale(0);
+          /* 0.375 = 75vh / 200vh */
+          animation: ${scale(1)} ${redLineAnimationProps.duration_ms / 2}ms
+            ease-in-out
+            ${redLineAnimationProps.delay_ms +
+            redLineAnimationProps.duration_ms * 0.375}ms
+            forwards;
+        `
+      : css`
+          transform: scale(1);
+          animation: ${scale(0)} ${redLineAnimationProps.duration_ms / 2}ms
+            ease-in-out
+            ${redLineAnimationProps.delay_ms +
+            redLineAnimationProps.duration_ms * 0.375}ms
+            forwards;
+        `}
 `;
 
-const BlackCircle = styled.div`
+const BlackCircle = styled.div<{ isStartDisappear: boolean }>`
   position: absolute;
   top: 84px;
   right: 13.75vw;
@@ -78,16 +103,29 @@ const BlackCircle = styled.div`
   height: 74px;
   background-color: ${ANGE_BLACK};
   border-radius: 50%;
-  transform: scale(0);
-  /* 0.06875 = 13.75vw / 200vw */
-  animation: ${scale(1)} ${blackLineAnimationProps.duration_ms / 2}ms
-    ease-in-out
-    ${blackLineAnimationProps.delay_ms +
-    blackLineAnimationProps.duration_ms * 0.06875}ms
-    forwards 2 alternate;
+  ${({ isStartDisappear }) =>
+    !isStartDisappear
+      ? css`
+          transform: scale(0);
+          /* 0.06875 = 13.75vw / 200vw */
+          animation: ${scale(1)} ${blackLineAnimationProps.duration_ms / 2}ms
+            ease-in-out
+            ${blackLineAnimationProps.delay_ms +
+            blackLineAnimationProps.duration_ms * 0.06875}ms
+            forwards;
+        `
+      : css`
+          transform: scale(1);
+          /* 0.06875 = 13.75vw / 200vw */
+          animation: ${scale(0)} ${blackLineAnimationProps.duration_ms / 2}ms
+            ease-in-out
+            ${blackLineAnimationProps.delay_ms +
+            blackLineAnimationProps.duration_ms * 0.06875}ms
+            forwards;
+        `}
 `;
 
-const YellowLine = styled.div`
+const YellowLine = styled.div<{ isStartDisappear: boolean }>`
   position: absolute;
   top: 40px;
   right: 100vw;
@@ -96,10 +134,10 @@ const YellowLine = styled.div`
   background-color: ${ANGE_YELLOW};
   animation: ${translate({ x: 0, y: 0 }, { x: "200vw", y: 0 })}
     ${yellowLineAnimationProps.duration_ms}ms linear
-    ${yellowLineAnimationProps.delay_ms}ms both 2 alternate;
+    ${yellowLineAnimationProps.delay_ms}ms both;
 `;
 
-const RedLine = styled.div`
+const RedLine = styled.div<{ isStartDisappear: boolean }>`
   position: absolute;
   top: 100vh;
   right: calc(47.3vw - 51px);
@@ -108,10 +146,10 @@ const RedLine = styled.div`
   background-color: ${ANGE_LIVE_BACK_COLOR};
   animation: ${translate({ x: 0, y: 0 }, { x: 0, y: "-200vh" })}
     ${redLineAnimationProps.duration_ms}ms linear
-    ${redLineAnimationProps.delay_ms}ms both 2 alternate;
+    ${redLineAnimationProps.delay_ms}ms both;
 `;
 
-const BlackLine = styled.div`
+const BlackLine = styled.div<{ isStartDisappear: boolean }>`
   position: absolute;
   top: 121px;
   right: -100vw;
@@ -120,19 +158,32 @@ const BlackLine = styled.div`
   background-color: ${ANGE_BLACK};
   animation: ${translate({ x: 0, y: 0 }, { x: "-200vw", y: 0 })}
     ${blackLineAnimationProps.duration_ms}ms linear
-    ${blackLineAnimationProps.delay_ms}ms both 2 alternate;
+    ${blackLineAnimationProps.delay_ms}ms both;
 `;
 
 const RightTopCircles: React.VFC<Props> = ({ isStartAnimation }) => {
+  const [isStartDisappear, changeIsStartDisappear] = useState<boolean>(false);
   if (isStartAnimation) {
     return (
       <Wrapper>
-        <YellowLine />
-        <RedLine />
-        <BlackLine />
-        <RedCircle />
-        <BlackCircle />
-        <YellowCircle />
+        <YellowLine
+          key={`${isStartDisappear}`}
+          isStartDisappear={isStartDisappear}
+        />
+        <RedLine
+          key={`${isStartDisappear}`}
+          isStartDisappear={isStartDisappear}
+        />
+        <BlackLine
+          key={`${isStartDisappear}`}
+          isStartDisappear={isStartDisappear}
+        />
+        <RedCircle isStartDisappear={isStartDisappear} />
+        <BlackCircle
+          isStartDisappear={isStartDisappear}
+          onAnimationEnd={() => changeIsStartDisappear(true)}
+        />
+        <YellowCircle isStartDisappear={isStartDisappear} />
       </Wrapper>
     );
   } else {
