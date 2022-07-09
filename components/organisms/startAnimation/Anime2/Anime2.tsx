785c70a1d2b47f4b513a92097794b8a35976a5a9
UMASHIBA <umashiba1@gmail.com>
2022-07-10 01:25:53 +0900

    add: create circle to wave animation

--- components/organisms/startAnimation/Anime2/Anime2.tsx
+++ components/organisms/startAnimation/Anime2/Anime2.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import { ANGE_WHITE } from "../../../../constants/colors";
 import ExpandCircle from "./ExpandCircle";
-import RedScreenToWave from "./RedScreenToWave";
 
 interface Props {
   isStartAnimation: boolean;
@@ -22,7 +21,6 @@ const Anime2: React.VFC<Props> = ({ isStartAnimation }) => {
     return (
       <Wrapper>
         <ExpandCircle />
-        <RedScreenToWave />
       </Wrapper>
     );
   } else {
--- components/organisms/startAnimation/Anime2/ExpandCircle.tsx
+++ components/organisms/startAnimation/Anime2/ExpandCircle.tsx
@@ -1,25 +1,72 @@
-import React from "react";
-import styled from "styled-components";
-import { ANGE_LIVE_BACK_COLOR } from "../../../../constants/colors";
+import React, { ReactNode } from "react";
+import styled, { css } from "styled-components";
+import {
+  ANGE_BLACK,
+  ANGE_LIVE_BACK_COLOR,
+  ANGE_YELLOW,
+  ANGE_WHITE,
+} from "../../../../constants/colors";
 import { scale } from "../../../../styles/commonAnimation";
+import { RedBlackYellow } from "../../../../typing/Color";
 
-const RedCircle = styled.div`
+interface Props {
+  children?: ReactNode;
+}
+
+const Circle = styled.div<{
+  color: RedBlackYellow | typeof ANGE_LIVE_BACK_COLOR | typeof ANGE_WHITE;
+  delayMs: number;
+  firstScale?: number;
+  position: "left" | "center" | "right";
+}>`
   position: absolute;
-  top: calc(50% - 85px);
-  left: calc(50% - 85px);
+  ${({ position }) =>
+    position === "left" &&
+    css`
+      top: calc(50% - 85px);
+      left: calc(50% - 275px);
+    `}
+  ${({ position }) =>
+    position === "center" &&
+    css`
+      top: calc(50% - 85px);
+      left: calc(50% - 85px);
+    `}
+  ${({ position }) =>
+    position === "right" &&
+    css`
+      top: calc(50% - 85px);
+      left: calc(50% + 105px);
+    `}
   display: flex;
   align-items: center;
   justify-content: center;
   width: 170px;
   height: 170px;
   border-radius: 50%;
-  background-color: ${ANGE_LIVE_BACK_COLOR};
-  transform: scale(1);
-  animation: ${scale(8)} 300ms cubic-bezier(0.75, -0.5, 0, 1) 0ms forwards;
+  background-color: ${({ color }) => color};
+  transform: scale(${({ firstScale = 0 }) => firstScale});
+  ${({ delayMs }) => css`
+    animation: ${scale(14)} 300ms ease-out ${delayMs}ms forwards;
+  `}
 `;
 
-const ExpandCircle: React.VFC = () => {
-  return <RedCircle />;
+const ExpandCircle: React.VFC<Props> = ({ children }) => {
+  return (
+    <>
+      <Circle
+        color={ANGE_LIVE_BACK_COLOR}
+        position="center"
+        firstScale={1}
+        delayMs={0}
+      />
+      <Circle color={ANGE_BLACK} position="left" delayMs={100} />
+      <Circle color={ANGE_YELLOW} position="right" delayMs={200} />
+      <Circle color={ANGE_WHITE} position="center" delayMs={300}>
+        {children}
+      </Circle>
+    </>
+  );
 };
 
 export default ExpandCircle;
