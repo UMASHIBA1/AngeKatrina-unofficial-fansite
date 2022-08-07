31de531729d325eb4f6542886878586b506f481e
UMASHIBA <umashiba1@gmail.com>
2022-08-07 13:45:14 +0900

    mod: create inScreen animation base

--- components/organisms/startAnimation/Anime2/CloseAnime2.tsx
+++ components/organisms/startAnimation/Anime2/CloseAnime2.tsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import styled, { css, keyframes } from "styled-components";
 import { ANGE_LIVE_BACK_COLOR } from "../../../../constants/colors";
 
 const Wrapper = styled.div`
   position: absolute;
-  top: -25%;
-  left: 0;
+  top: -50%;
+  left: -50%;
   display: flex;
   flex-direction: row;
-  width: 100%;
-  height: 150%;
+  width: 200%;
+  height: 200%;
   transform: rotate(45deg);
 `;
 
@@ -18,10 +18,27 @@ const inScreenAnimation = (position: "left" | "right") => {
 
   return keyframes`
 
+    0% {
+        transform: translateX(${prefix}160%);
+    }
+
     100% {
-        transform: translateX(${prefix}100%);
+        transform: translateX(${prefix}50%);
     }
 `;
+};
+
+const closingAnimation = (position: "left" | "right") => {
+  const prefix = position === "left" ? "-" : "";
+  return keyframes`
+        0% {
+            transform: translateX(${prefix}50%);
+        }
+
+        100% {
+            transform: translateX(0);
+        }
+    `;
 };
 
 const RedCloser = styled.div<{
@@ -31,29 +48,44 @@ const RedCloser = styled.div<{
   width: 50%;
   height: 100%;
   background-color: ${ANGE_LIVE_BACK_COLOR};
-  ${({ position }) =>
-    position === "left" &&
-    css`
-      transform: translateX(-160%);
-    `}
-  ${({ position }) =>
-    position === "right" &&
-    css`
-      transform: translateX(160%);
-    `}
-    ${({ animationKind, position }) =>
+  ${({ animationKind, position }) =>
     animationKind === "inScreen" &&
     css`
       animation: ${inScreenAnimation(position)} 600ms
-        cubic-bezier(0.1, 0.46, 0.22, 1.05) 2000ms forwards;
+        cubic-bezier(0.1, 0.46, 0.22, 1) 2000ms both;
+    `}
+  ${({ animationKind, position }) =>
+    animationKind === "closing" &&
+    css`
+      animation: ${closingAnimation(position)} 600ms ease-in-out 400ms both;
     `}
 `;
 
+const useAnimation = () => {
+  const [animationkind, changeAnimationKind] = useState<"inScreen" | "closing">(
+    "inScreen"
+  );
+  const toNextAnimation = () => {
+    changeAnimationKind("closing");
+  };
+
+  return [animationkind, toNextAnimation] as [
+    typeof animationkind,
+    typeof toNextAnimation
+  ];
+};
+
 const CloseAnime2: React.VFC = () => {
+  const [animationKind, toNextAnimation] = useAnimation();
+
   return (
     <Wrapper>
-      <RedCloser position="left" animationKind="inScreen" />
-      <RedCloser position="right" animationKind="inScreen" />
+      <RedCloser
+        position="left"
+        animationKind={animationKind}
+        onAnimationEnd={toNextAnimation}
+      />
+      <RedCloser position="right" animationKind={animationKind} />
     </Wrapper>
   );
 };
