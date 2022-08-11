9726a52f2bdbe55b498ba92f120f749773d48648
UMASHIBA <umashiba1@gmail.com>
2022-08-12 01:04:06 +0900

    mod: create anime3 open animation

--- components/organisms/startAnimation/Anime3/OpenAnime3.tsx
+++ components/organisms/startAnimation/Anime3/OpenAnime3.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import styled, { css } from "styled-components";
-import { ANGE_LIVE_BACK_COLOR } from "../../../../constants/colors";
+import { ANGE_LIVE_BACK_COLOR, ANGE_WHITE } from "../../../../constants/colors";
 import { translate } from "../../../../styles/commonAnimation";
 
 const Wrapper = styled.div`
@@ -11,32 +11,70 @@ const Wrapper = styled.div`
   flex-direction: row;
   width: 300%;
   height: 300%;
-  transform: rotate(-45deg);
+  transform: rotate(-36deg);
+  background-color: ${ANGE_WHITE};
 `;
 
-const RedOpener = styled.div<{ position: "left" | "right" }>`
+const RedOpener = styled.div<{
+  position: "left" | "right";
+  animationKind: "open" | "expand";
+}>`
   width: 50%;
   height: 100%;
   background-color: ${ANGE_LIVE_BACK_COLOR};
-  ${({ position }) =>
+  ${({ position, animationKind }) =>
     position === "left" &&
+    animationKind === "open" &&
     css`
-      animation: ${translate({ x: 0, y: 0 }, { x: "-25%", y: 0 })} 400ms ease-in
-        100ms both;
+      animation: ${translate({ x: 0, y: 0 }, { x: "-32px", y: 0 })} 400ms
+        ease-in 400ms both;
     `}
-  ${({ position }) =>
-    position === "right" &&
+  ${({ position, animationKind }) =>
+    position === "left" &&
+    animationKind === "expand" &&
     css`
-      animation: ${translate({ x: 0, y: 0 }, { x: "25%", y: 0 })} 400ms ease-in
-        100ms both;
+      animation: ${translate({ x: "-32px", y: 0 }, { x: "-25%", y: 0 })} 200ms
+        cubic-bezier(0, 1, 0.5, 1.25) 400ms both;
+    `}
+  ${({ position, animationKind }) =>
+    position === "right" &&
+    animationKind === "open" &&
+    css`
+      animation: ${translate({ x: 0, y: 0 }, { x: "32px", y: 0 })} 400ms ease-in
+        400ms both;
+    `}
+    ${({ position, animationKind }) =>
+    position === "right" &&
+    animationKind === "expand" &&
+    css`
+      animation: ${translate({ x: "32px", y: 0 }, { x: "25%", y: 0 })} 200ms
+        cubic-bezier(0, 1, 0.5, 1.25) 400ms both;
     `}
 `;
 
+const useAnimation = () => {
+  const [animationKind, changeAnimationKind] = useState<"open" | "expand">(
+    "open"
+  );
+  const toNextAnimation = () => {
+    changeAnimationKind("expand");
+  };
+  return [animationKind, toNextAnimation] as [
+    typeof animationKind,
+    typeof toNextAnimation
+  ];
+};
+
 const OpenAnime3: React.VFC = () => {
+  const [animationKind, toNextAnimation] = useAnimation();
   return (
     <Wrapper>
-      <RedOpener position="left" />
-      <RedOpener position="right" />
+      <RedOpener position="left" animationKind={animationKind} />
+      <RedOpener
+        position="right"
+        animationKind={animationKind}
+        onAnimationEnd={toNextAnimation}
+      />
     </Wrapper>
   );
 };
