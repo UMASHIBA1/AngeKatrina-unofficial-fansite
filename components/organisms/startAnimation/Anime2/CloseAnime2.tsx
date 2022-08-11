172ec22c1092241be29e709caca41966ed71f9ca
UMASHIBA <umashiba1@gmail.com>
2022-08-11 23:46:40 +0900

    mod: improve close anime2 animation

--- components/organisms/startAnimation/Anime2/CloseAnime2.tsx
+++ components/organisms/startAnimation/Anime2/CloseAnime2.tsx
@@ -1,19 +1,30 @@
 import React, { useState } from "react";
 import styled, { css, keyframes } from "styled-components";
+import {
+  sm_breakpoint,
+  tablet_breakpoint,
+} from "../../../../constants/breakpoints";
 import { ANGE_LIVE_BACK_COLOR } from "../../../../constants/colors";
+import { leftRotate } from "../../../../styles/commonAnimation";
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{ isStartAnimation: boolean }>`
   position: absolute;
-  top: -50%;
-  left: -50%;
+  top: -100%;
+  left: -100%;
   display: flex;
   flex-direction: row;
-  width: 200%;
-  height: 200%;
+  width: 300%;
+  height: 300%;
   transform: rotate(45deg);
+  ${({ isStartAnimation }) =>
+    isStartAnimation &&
+    css`
+      animation: ${leftRotate("45deg", "315deg")} 400ms ease-in-out 400ms both;
+    `}
+  transform-origin: center;
 `;
 
-const inScreenAnimation = (position: "left" | "right") => {
+const inScreenAnimation = (position: "left" | "right", stopPercent: number) => {
   const prefix = position === "left" ? "-" : "";
 
   return keyframes`
@@ -23,16 +34,16 @@ const inScreenAnimation = (position: "left" | "right") => {
     }
 
     100% {
-        transform: translateX(${prefix}50%);
+        transform: translateX(${prefix}${stopPercent}%);
     }
 `;
 };
 
-const closingAnimation = (position: "left" | "right") => {
+const closingAnimation = (position: "left" | "right", stopPercent: number) => {
   const prefix = position === "left" ? "-" : "";
   return keyframes`
         0% {
-            transform: translateX(${prefix}50%);
+            transform: translateX(${prefix}${stopPercent}%);
         }
 
         100% {
@@ -51,13 +62,33 @@ const RedCloser = styled.div<{
   ${({ animationKind, position }) =>
     animationKind === "inScreen" &&
     css`
-      animation: ${inScreenAnimation(position)} 600ms
+      animation: ${inScreenAnimation(position, 50)} 600ms
         cubic-bezier(0.1, 0.46, 0.22, 1) 2000ms both;
+
+      @media (min-width: ${sm_breakpoint}px) {
+        animation: ${inScreenAnimation(position, 30)} 600ms
+          cubic-bezier(0.1, 0.46, 0.22, 1) 2000ms both;
+      }
+
+      @media (min-width: ${tablet_breakpoint}px) {
+        animation: ${inScreenAnimation(position, 25)} 600ms
+          cubic-bezier(0.1, 0.46, 0.22, 1) 2000ms both;
+      }
     `}
   ${({ animationKind, position }) =>
     animationKind === "closing" &&
     css`
-      animation: ${closingAnimation(position)} 600ms ease-in-out 400ms both;
+      animation: ${closingAnimation(position, 50)} 400ms ease-in-out 400ms both;
+
+      @media (min-width: ${sm_breakpoint}px) {
+        animation: ${closingAnimation(position, 30)} 400ms ease-in-out 400ms
+          both;
+      }
+
+      @media (min-width: ${tablet_breakpoint}px) {
+        animation: ${closingAnimation(position, 25)} 400ms ease-in-out 400ms
+          both;
+      }
     `}
 `;
 
@@ -79,7 +110,7 @@ const CloseAnime2: React.VFC = () => {
   const [animationKind, toNextAnimation] = useAnimation();
 
   return (
-    <Wrapper>
+    <Wrapper isStartAnimation={animationKind === "closing"}>
       <RedCloser
         position="left"
         animationKind={animationKind}
