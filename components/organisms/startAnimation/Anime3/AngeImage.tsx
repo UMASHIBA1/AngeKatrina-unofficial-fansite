9e48cf2112f12d4f1e9d6dfd43a121e1e0e2adb1
UMASHIBA <umashiba1@gmail.com>
2022-08-15 22:32:03 +0900

    mod: add animation to ange img

--- components/organisms/startAnimation/Anime3/AngeImage.tsx
+++ components/organisms/startAnimation/Anime3/AngeImage.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 import angeHey from "../../../../public/imgs/gatya/ange-hey.png";
+import { scale } from "../../../../styles/commonAnimation";
 
 const Img = styled.img`
   object-fit: contain;
   height: 120%;
   margin-top: 30%;
+  transform-origin: center;
+  animation: ${scale(1.2)} 200ms ease-out 1200ms 2 alternate both;
 `;
-
 const AngeImage: React.VFC = () => {
   return <Img src={angeHey} alt="アンジュのかわいい立ち絵" />;
 };
